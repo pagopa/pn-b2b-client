eaa82864d67eeb8d6fc02bae57486582ab2d9da7
LorisEndrizzi <loris.endrizzi@microtelinnovation.com>
2023-03-13 17:03:26 +0100

    PN-4675 aggiunto test invio analogico per PF

--- src/test/java/it/pagopa/pn/cucumber/steps/SharedSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/SharedSteps.java
@@ -154,6 +154,14 @@ public class SharedSteps {
                         .taxId(marioGherkinTaxID));
     }
 
+    @And("destinatario Mario Gherkin con taxid {string}:")
+    public void destinatarioMarioGherkinCFParam(String taxId ,@Transpose NotificationRecipient recipient) {
+        this.notificationRequest.addRecipientsItem(
+                recipient
+                        .denomination("Mario Gherkin")
+                        .taxId(taxId));
+    }
+
 
     @And("destinatario Gherkin spa")
     public void destinatarioGherkinSpa() {
