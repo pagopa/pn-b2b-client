3c164ebb31a69c55663cf02a4b70e3bb0614f94a
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-07-11 14:13:05 +0200

    esecuzione parallela test lite

--- src/test/java/it/pagopa/pn/cucumber/CucumberLiteIntegrationTest.java
+++ src/test/java/it/pagopa/pn/cucumber/CucumberLiteIntegrationTest.java
@@ -1,20 +1,19 @@
 package it.pagopa.pn.cucumber;
 
-
 import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("it/pagopa/pn/cucumber/Allegati.feature")
+@SelectClasspathResource("it/pagopa/pn/cucumber")
 @ConfigurationParameters({
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-retention-allegati-report.json"),
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-retention-allegati-reporthtml"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-lite-report.json"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-lite-report.html"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
-@ExcludeTags({"ignore"})
-public class CucumberTestAllegati {
+@IncludeTags({"testLite"})
+public class CucumberLiteIntegrationTest {
 }
