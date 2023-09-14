460d3e44e19cfba2081911e0792819c7cf199cad
angelo.minisci <angelo.minisci@gmail.com>
2023-09-14 16:04:40 +0200

    PN-7412 Refactor Test

---
+++ src/test/java/it/pagopa/pn/cucumber/CucumberLiteIntegrationAnnullamentoPGTest.java
@@ -0,0 +1,20 @@
+package it.pagopa.pn.cucumber;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("it/pagopa/pn/cucumber/annullamentoNotifica/AnnullamentoNotificheB2bPG.feature")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
+                "html:target/cucumber-lite-Annullamento-DEV-14092023_1-report.html," +
+                "json:target/cucumber-lite-Annullamento-DEV-14092023_1-report.json"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
+        @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
+})
+@ExcludeTags({"ignore"})
+public class CucumberLiteIntegrationAnnullamentoPGTest {
+}
