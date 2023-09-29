f39b6ed16bcdda685ff0f2240c6ac897ac129aa7
angelo.minisci <angelo.minisci@gmail.com>
2023-09-29 07:05:55 +0200

    PN-8090 Refactor

--- src/test/java/it/pagopa/pn/cucumber/steps/pa/AvanzamentoNotificheB2bSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/pa/AvanzamentoNotificheB2bSteps.java
@@ -1273,8 +1273,8 @@ public class AvanzamentoNotificheB2bSteps {
             sharedSteps.selectUser(user);
             downloadLegalFact(legalFactCategory, false, false, true, null);
         } catch (AssertionFailedError assertionFailedError) {
-            System.out.println(assertionFailedError.getCause().toString());
-            System.out.println(assertionFailedError.getCause().getMessage().toString());
+           // System.out.println(assertionFailedError.getCause().toString());
+           // System.out.println(assertionFailedError.getCause().getMessage().toString());
             Assertions.assertTrue(assertionFailedError.getCause().getMessage().toString().substring(0, 3).equals(statusCode));
         }
     }
