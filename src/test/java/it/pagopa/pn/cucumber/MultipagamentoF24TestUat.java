77d8b6574ac194091eabe4e6816c8676e7d0ae67
angelo.minisci <angelo.minisci@gmail.com>
2023-11-06 11:59:29 +0100

    remove configuration main class

--- src/test/java/it/pagopa/pn/cucumber/MultipagamentoF24TestUat.java
+++ src/test/java/it/pagopa/pn/cucumber/MultipagamentoF24TestUat.java
@@ -13,7 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
                 "html:target/cucumber-report.html"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
-        @ConfigurationParameter(key = PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME, value = "90"),
 })
 
 @ExcludeTags({"ignore", "mockPec"})
