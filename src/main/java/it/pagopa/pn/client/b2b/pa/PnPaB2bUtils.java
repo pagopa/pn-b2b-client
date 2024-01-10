0e711774fc66363df914b083a67ccb52b2a78cbb
Marco Meloni <marco.meloni@cinetix-team.it>
2024-01-10 15:47:49 +0100

    aumento depth per caricamento degli allegati

--- src/main/java/it/pagopa/pn/client/b2b/pa/PnPaB2bUtils.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/PnPaB2bUtils.java
@@ -1273,7 +1273,7 @@ public class PnPaB2bUtils {
 
 
     private void loadToPresigned( String url, String secret, String sha256, String resource,String resourceType, int depth ) {
-        if(depth >= 5){
+        if(depth >= 8){
             throw new ResourceAccessException("max depth, PUT not working");
         }
         try{
