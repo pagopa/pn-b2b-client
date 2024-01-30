c27b7585220b8193a5b73fc06b4155bf52b0386c
Marco Meloni <marco.meloni@cinetix-team.it>
2024-01-30 11:42:43 +0100

    aggiunta nuovo campo vat alla notifica

--- src/test/java/it/pagopa/pn/cucumber/utils/NotificationValue.java
+++ src/test/java/it/pagopa/pn/cucumber/utils/NotificationValue.java
@@ -28,6 +28,7 @@ public enum NotificationValue {
     TAXONOMY_CODE("taxonomyCode","010202N",false),
     AMOUNT("amount",null,false),
     PA_FEE("paFee",null,false),
+    VAT("vat",null,false),
     PAYMENT_EXPIRATION_DATE("paymentExpirationDate", null,false),
 
     /*RECIPIENT*/
