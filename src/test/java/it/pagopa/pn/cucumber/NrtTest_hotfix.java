4b4da2bab1f138de989a6262b12f432dab63fab5
Marco Meloni <marco.meloni@cinetix-team.it>
2024-03-21 16:23:09 +0100

    rimozione 120 f24 dalle run NRT

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
@@ -14,7 +14,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
-@ExcludeTags({"ignore","uat","appIo","realNormalizzatore","integration","realNR","120F24"})
+@ExcludeTags({"ignore","uat","appIo","realNormalizzatore","integration","realNR"})
 @IncludeTags({"workflowDigitale", "workflowAnalogico", "pagamentiMultipli",
         "Async", "f24", "version","AOO_UO", "Annullamento",
         "validation", "RetentionAllegati", "apiKeyManager", "downtimeLogs",
