fc888bf2c6a62f798b64dca2783998308deb5fe2
Matteo Sperati <matteo.sperati@dgsspa.com>
2024-09-19 16:16:53 +0200

    19/09/2024
    Rinomina delle suite di IndicizzazioneSafeStorage per maggior chiarezza

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
@@ -18,6 +18,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeTags({"workflowDigitale", "workflowAnalogico", "pagamentiMultipli",
         "Async", "f24", "version", "AOO_UO", "Annullamento",
         "validation", "RetentionAllegati", "apiKeyManager", "downtimeLogs",
-        "legalFact", "letturaDestinatario", "hotfix-11281", "indicizzazioneSafeStorage", "uatIndexSS"})
+        "legalFact", "letturaDestinatario", "hotfix-11281", "indicizzazioneSafeStorage"})
 public class NrtTest_hotfix {
 }
