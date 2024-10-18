38972d4643bb4e277fd5f2ea510ccf036ed1b2f0
Matteo Mancini <matteo.mancini@alten.it>
2024-10-18 09:44:14 +0200

    update PGAuthentication.java to run multiple scenarios

--- src/test/java/it/pagopa/pn/cucumber/PGAuthentication.java
+++ src/test/java/it/pagopa/pn/cucumber/PGAuthentication.java
@@ -14,7 +14,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
-@IncludeTags({"pgAuthentication"})
+@IncludeTags({"pgAuthentication", "raddTechnicalAnnex"})
 @ExcludeTags({"ignore"})
 public class PGAuthentication {
 }
