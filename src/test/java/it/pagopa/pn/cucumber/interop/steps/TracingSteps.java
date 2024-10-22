987456a5a6197247b00d2c5c41271eb065ff14eb
Matteo Mancini <matteo.mancini@alten.it>
2024-10-22 12:36:18 +0200

    QA-22: test scenario implemented up to number 10

--- src/test/java/it/pagopa/pn/cucumber/interop/steps/TracingSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/interop/steps/TracingSteps.java
@@ -3,48 +3,137 @@ package it.pagopa.pn.cucumber.interop.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import it.pagopa.interop.client.b2b.generated.openapi.clients.interop.model.SubmitTracingResponse;
+import it.pagopa.interop.client.b2b.generated.openapi.clients.interop.model.*;
 import it.pagopa.pn.client.b2b.pa.service.interop.IInteropTracingClient;
 import it.pagopa.pn.client.b2b.pa.service.utils.SettableBearerToken;
 import it.pagopa.pn.cucumber.interop.domain.TracingCsvFile;
 import org.junit.jupiter.api.Assertions;
 import org.opentest4j.AssertionFailedError;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class TracingSteps {
     private final IInteropTracingClient interopTracingClient;
     private TracingCsvFile tracingCsvFile;
     private SubmitTracingResponse submitTracingResponse;
+    private GetTracingsResponse getTracingsResponse;
+    private GetTracingErrorsResponse getTracingErrorsResponse;
     private ResourceLoader resourceLoader;
+    private Resource resource;
+    private HttpStatusCodeException httpStatusCodeException;
 
     public TracingSteps(IInteropTracingClient interopTracingClient, ResourceLoader resourceLoader) {
         this.interopTracingClient = interopTracingClient;
         this.resourceLoader = resourceLoader;
     }
 
-    @Given("l'ente {string} prepara un nuovo file CSV")
-    public void createCsv(String operator) {
+    @Given("l'ente {string} prepara il file CSV {string}")
+    public void createCsv(String operator, String file) {
         selectOperator(operator);
         //TODO create csv at runtime or fixed csv?
-//        tracingCsv =
-
+        resource = resourceLoader.getResource(selectCsvFile(file));
     }
 
-    @When("viene sottomesso il file CSV")
-    public void uploadCsv() {
+    @When("viene sottomesso il file CSV in data {string}")
+    public void uploadCsv(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
         try {
-            submitTracingResponse = interopTracingClient.submitTracing(resourceLoader.getResource(""), LocalDate.now());
+            submitTracingResponse = interopTracingClient.submitTracing(resource, LocalDate.parse(date, formatter));
+        } catch (HttpStatusCodeException statusCodeException) {
+            httpStatusCodeException = statusCodeException;
         } catch (Exception ex) {
             throw new AssertionFailedError("There was an error while submitting the tracing csv: " + ex);
         }
     }
 
-    @Then("il caricamento del csv viene effettuato con successo")
-    public void verifyUploadSuccessfully() {
+    @When("viene recuperata la lista di tracing con uno stato tra i seguenti$")
+    public void retrieveTracingByStatusList(List<String> statusList) {
+        List<TracingState> tracingStates = statusList.stream().map(TracingState::fromValue).toList();
+        retrieveTracing(tracingStates);
+    }
+
+    @When("viene recuperata la lista di tracing con stato {string}")
+    public void retrieveTracingByStatus(String status) {
+        retrieveTracing(List.of(TracingState.fromValue(status)));
+    }
+
+    public void retrieveTracing(List<TracingState> statusList) {
+        try {
+            getTracingsResponse = interopTracingClient.getTracings(0, 30, statusList);
+        } catch (HttpStatusCodeException statusCodeException) {
+            httpStatusCodeException = statusCodeException;
+        } catch (Exception ex) {
+            throw new AssertionFailedError("There was an error while retrieving the tracings: " + ex);
+        }
+    }
+
+    @Then("la risposta contiene soltanto i tracing con stato {string}")
+    public void verififyGetTracingResponse(String status) {
+        Assertions.assertNotNull(getTracingsResponse, "There was an error while retrieving the getTracing response!");
+        Assertions.assertFalse(getTracingsResponse.getResults().stream().anyMatch(x -> !Objects.equals(x.getState(), status)));
+    }
+
+    @Then("non viene trovato nessun tracing caricato")
+    public void verifyGetTracingResponse() {
+        Assertions.assertNotNull(getTracingsResponse, "There was an error while retrieving the getTracing response!");
+        Assertions.assertTrue(getTracingsResponse.getResults().isEmpty());
+    }
+
+    @Then("^il caricamento del csv viene effettuato con errori")
+    public void verifyUploadWithErrors() {
+        verifyUpload(true);
+    }
+
+    @Then("^il caricamento del csv viene effettuato senza errori")
+    public void verifyUploadWithoutErrors() {
+        verifyUpload(false);
+    }
+
+    private void verifyUpload(boolean hasErrors) {
         Assertions.assertNotNull(submitTracingResponse, "There was an error while retrieving the tracing response!");
-        Assertions.assertFalse(submitTracingResponse.getErrors());
+        Assertions.assertNotNull(submitTracingResponse.getErrors());
+        Assertions.assertEquals(hasErrors, submitTracingResponse.getErrors());
+    }
+
+    @Then("la chiamata fallisce con status code: {int}")
+    public void checkStatusCode(int statusCode) {
+        Assertions.assertEquals(statusCode, httpStatusCodeException.getStatusCode().value());
+    }
+
+    @When("viene recuperato il dettaglio del tracing con errori")
+    public void retrieveTracingError() {
+        getTracingErrors(submitTracingResponse.getTracingId());
+    }
+
+    @Then("viene recuperato il dettaglio degli errori per il tracing {string}")
+    public void retrieveSpecificTracingError(String tracingId) {
+        getTracingErrors(UUID.fromString(tracingId));
+    }
+
+    private void getTracingErrors(UUID tracingId) {
+        try {
+            getTracingErrorsResponse = interopTracingClient.getTracingErrors(tracingId, 0, 30);
+        } catch (HttpStatusCodeException statusCodeException) {
+            httpStatusCodeException = statusCodeException;
+        } catch (Exception ex) {
+            throw new AssertionFailedError("There was an error while retrieving the tracing error: " + ex);
+        }
+    }
+
+    @Then("il dettaglio ritorna gli errori aspettati")
+    public void verifyGetTracingErrorResponse() {
+        Assertions.assertNotNull(getTracingErrorsResponse, "There was an error while retrieving the tracing error!");
+        Assertions.assertNotNull(getTracingErrorsResponse.getResults());
+        List<GetTracingErrorsResponseResults> expectedResult = List.of(
+                createExpectedResponse("errorCode", "message", "purposeId", 10),
+                createExpectedResponse("errorCode", "message", "purposeId", 10)
+        );
+        Assertions.assertEquals(expectedResult, getTracingErrorsResponse.getResults());
     }
 
     private void selectOperator(String operator) {
@@ -53,5 +142,22 @@ public class TracingSteps {
             case "operator2" -> interopTracingClient.setBearerToken(SettableBearerToken.BearerTokenType.PG_1);
             default -> throw new IllegalStateException("Unexpected value: " + operator.trim().toLowerCase());
         }
+    }
+
+    private String selectCsvFile(String file) {
+        return switch (file.trim().toLowerCase()) {
+            case "corretto" -> "classpath:";
+            case "non valido" -> "classpath:";
+            default -> throw new IllegalStateException("Unexpected value: " + file.trim().toLowerCase());
+        };
+    }
+
+    private GetTracingErrorsResponseResults createExpectedResponse(String errorCode, String message, String purposeId, Integer rowNumber) {
+        GetTracingErrorsResponseResults tracingErrorsResponse = new GetTracingErrorsResponseResults();
+        tracingErrorsResponse.setErrorCode(errorCode);
+        tracingErrorsResponse.setMessage(message);
+        tracingErrorsResponse.setPurposeId(purposeId);
+        tracingErrorsResponse.setRowNumber(rowNumber);
+        return tracingErrorsResponse;
     }
 }
--- src/test/resources/it/pagopa/pn/cucumber/interop/InteropTracing.feature
+++ src/test/resources/it/pagopa/pn/cucumber/interop/InteropTracing.feature
@@ -1,6 +1,47 @@
 Feature: Interop Tracing feature
 
-  Scenario: [] Inserimento di un nuovo file CSV di tracing giornaliero
-    Given l'ente "" prepara un nuovo file CSV
-    When viene sottomesso il file CSV
-    Then il caricamento del csv viene effettuato con successo
+  Scenario: [1] Inserimento di un nuovo file CSV di tracing giornaliero
+    Given l'ente "" prepara il file CSV "CORRETTO"
+    When viene sottomesso il file CSV in data "2024-10-22"
+    Then il caricamento del csv viene effettuato senza errori
+
+  Scenario: [2] Inserimento di un file CSV di tracing giornaliero per una data già presente
+    # PRIMO CARICAMENTO
+    Given l'ente "" prepara il file CSV "CORRETTO"
+    When viene sottomesso il file CSV in data "2024-10-22"
+    # SECONDO CARICAMENTO
+    Given l'ente "" prepara il file CSV "CORRETTO"
+    When viene sottomesso il file CSV in data "2024-10-22"
+    Then la chiamata fallisce con status code: 400
+
+  Scenario: [3] Inserimento di un nuovo file CSV di tracing giornaliero
+    Given l'ente "" prepara il file CSV "ERRATO"
+    When viene sottomesso il file CSV in data "2024-10-22"
+    Then il caricamento del csv viene effettuato con errori
+    # SCENARIO 9
+    When viene recuperato il dettaglio del tracing con errori
+    Then il dettaglio ritorna gli errori aspettati
+
+
+  Scenario Outline: [4-5-6-7] Recupero lista tracing con filtro stato
+    When viene recuperata la lista di tracing con stato "<status>"
+    Then la risposta contiene soltanto i tracing con stato "<status>"
+    Examples:
+      | status    |
+      | MISSING   |
+      | COMPLETED |
+      | PENDING   |
+      | ERROR     |
+
+  Scenario: [8] Recupero lista tracing per utenza dove non sia stato mai inserito alcun file
+#    Given l'ente "" effettua le chiamate
+    When viene recuperata la lista di tracing con uno stato tra i seguenti
+      | ERROR     |
+      | MISSING   |
+    Then non viene trovato nessun tracing caricato
+
+  Scenario: [10] Recupero dettaglio errori presenti nel file tracing
+    When viene recuperato il dettaglio degli errori per il tracing ""
+    Then la chiamata fallisce con status code: 400
+
+
