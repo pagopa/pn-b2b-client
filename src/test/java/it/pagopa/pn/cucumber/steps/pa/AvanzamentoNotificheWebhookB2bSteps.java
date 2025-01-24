a457ca75c040fcc69fedfa8782be979193ed52da
Tullio Ranieri <ranieritullio@gmail.com>
2025-01-24 17:05:41 +0100

    QA-4319 elimnati commenti

--- src/test/java/it/pagopa/pn/cucumber/steps/pa/AvanzamentoNotificheWebhookB2bSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/pa/AvanzamentoNotificheWebhookB2bSteps.java
@@ -2721,7 +2721,7 @@ public class AvanzamentoNotificheWebhookB2bSteps {
         Assertions.assertTrue(this.notificationError.getMessage().contains(errorMessage));
     }
 
-    //test da finire
+
     @And("vengono letti gli eventi dello stream del {string} fino allo stato {string} con versione V25")
     public void readStreamEventsStateV25(String pa, String status) {
         setPaWebhook(pa);
@@ -2737,32 +2737,6 @@ public class AvanzamentoNotificheWebhookB2bSteps {
 
         int numCheck = statusEventForStream.getNumCheck();
         int waiting = statusEventForStream.getWaiting();
-
-
-        /*
-
-
-        UUID uuid;
-        uuid = this.eventStreamListV23.get(0).getStreamId();
-
-        PnPollingResponseV25 pnPollingResponse = new PnPollingResponseV25();
-        NewNotificationRequestStatusResponseV23 statusResponseV23 = b2bClient.getNotificationRequestStatusV23();
-        pnPollingResponse.setStatusResponse(statusResponseV23);
-
-
-        if (pnPollingResponse.getStatusResponse().getIun() != null) {
-            FullSentNotificationV25 fullSentNotification;
-            try {
-                fullSentNotification = b2bClient.getSentNotificationV25(pnPollingResponse.getStatusResponse().getIun());
-            } catch (Exception exception) {
-                log.error("Error getPollingResponse(), Iun: {}, ApiKey: {}, PnPollingException: {}", pnPollingResponse.getStatusResponse().getIun(), b2bClient.getApiKeySetted().name(), exception.getMessage());
-                throw new PnPollingException(exception.getMessage());
-            }
-            pnPollingResponse.setNotification(fullSentNotification);
-
-        }
-
-        */
 
         ProgressResponseElementV24 progressResponseElement = null;
         boolean finded = false;
