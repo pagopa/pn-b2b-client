47f088d6dd49bbd3196682b0e431b4f21f72adaa
Matteo Mancini <matteo.mancini@alten.it>
2025-01-30 17:31:53 +0100

    QA-5675: updated token uuid

--- src/test/java/it/pagopa/pn/cucumber/interop/utility/TracingFileUtils.java
+++ src/test/java/it/pagopa/pn/cucumber/interop/utility/TracingFileUtils.java
@@ -24,8 +24,8 @@ public class TracingFileUtils {
             CSVWriter csvWriter = new CSVWriter(new FileWriter(file));
             csvWriter.writeNext(new String[] {"date", "purpose_id", "token", "status", "requests_count"});
             csvWriter.writeNext(new String[] {date, "28874634-6ea6-4def-b200-7377182c71be", "99382e29-b0cf-412b-a060-72e421b6d167", "200", "48"});
-            csvWriter.writeNext(new String[] {date, "28874634-6ea6-4def-b200-7377182c71be", "99382e29-b0cf-412b-a060-72e421b6d167", "404", "150"});
-            csvWriter.writeNext(new String[] {date, "28874634-6ea6-4def-b200-7377182c71be", "99382e29-b0cf-412b-a060-72e421b6d167", "500", "50"});
+            csvWriter.writeNext(new String[] {date, "28874634-6ea6-4def-b200-7377182c71be", "a7b2c3d4-e5f6-7890-abcd-ef1234567890", "404", "150"});
+            csvWriter.writeNext(new String[] {date, "28874634-6ea6-4def-b200-7377182c71be", "12345678-90ab-cdef-1234-567890abcdef", "500", "50"});
             csvWriter.close();
         } catch (IOException ex) {
             throw new AssertionFailedError("There was an error while generating the csv file: " + ex);
