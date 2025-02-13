7a372193816d1ce1684b3f5630aebdb1a683fc22
Tullio Ranieri <ranieritullio@gmail.com>
2025-02-13 14:26:29 +0100

    QA-5876 convert api paymentinfo in bff

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPaymentInfoClientImpl.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPaymentInfoClientImpl.java
@@ -1,9 +1,13 @@
 package it.pagopa.pn.client.b2b.pa.service.impl;
 
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.api.external.bff.payment.PaymentsApi;
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.payment.BffPaymentInfoItem;
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.payment.BffPaymentRequest;
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.payment.BffPaymentResponse;
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.payment.PaymentInfoRequest;
 import it.pagopa.pn.client.b2b.pa.service.IPnPaymentInfoClient;
-import it.pagopa.pn.client.b2b.web.generated.openapi.clients.payment_info.ApiClient;
-import it.pagopa.pn.client.b2b.web.generated.openapi.clients.payment_info.api.PaymentInfoApi;
-import it.pagopa.pn.client.b2b.web.generated.openapi.clients.payment_info.model.*;
+import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.api.external.bff.ApiClient;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -13,32 +17,31 @@ import java.util.List;
 
 @Component
 public class PnPaymentInfoClientImpl implements IPnPaymentInfoClient {
-    private final PaymentInfoApi paymentInfoApi;
+    private final PaymentsApi paymentInfoApi;
 
 
     public PnPaymentInfoClientImpl(RestTemplate restTemplate,
                                    @Value("${pn.webapi.external.base-url}") String deliveryBasePath ,
                                    @Value("${pn.bearer-token-payinfo}") String key) {
-        this.paymentInfoApi = new PaymentInfoApi(newApiClient( restTemplate, deliveryBasePath,key));
+        this.paymentInfoApi = new PaymentsApi(newApiClient( restTemplate, deliveryBasePath,key));
     }
 
     private static ApiClient newApiClient(RestTemplate restTemplate, String basePath, String key) {
         ApiClient newApiClient = new ApiClient( restTemplate );
         newApiClient.setBasePath( basePath );
-        newApiClient.setBearerToken(key);
+        newApiClient.addDefaultHeader("Authorization","Bearer " + key);
+
         return newApiClient;
     }
 
-    public PaymentInfo getPaymentInfo(String paTaxId, String noticeNumber) throws RestClientException {
-        return paymentInfoApi.getPaymentInfoWithHttpInfo(paTaxId, noticeNumber).getBody();
-    }
+
 
     @Override
-    public List<PaymentInfoV21> getPaymentInfoV21(List<PaymentInfoRequest> paymentInfoRequest) throws RestClientException {
-        return paymentInfoApi.getPaymentInfoV21WithHttpInfo(paymentInfoRequest).getBody();
+    public List<BffPaymentInfoItem> getPaymentInfoV21(List<PaymentInfoRequest> paymentInfoRequest) throws RestClientException {
+        return paymentInfoApi.getPaymentsInfoV1WithHttpInfo(paymentInfoRequest).getBody();
     }
 
-    public PaymentResponse checkoutCart(PaymentRequest paymentRequest) throws RestClientException {
-        return paymentInfoApi.checkoutCartWithHttpInfo(paymentRequest).getBody();
+    public BffPaymentResponse checkoutCart(BffPaymentRequest paymentRequest) throws RestClientException {
+        return paymentInfoApi.paymentsCartV1WithHttpInfo(paymentRequest).getBody();
     }
 }
