b771f4682060f5f645a2dac380c5bfc411a3f3b8
Matteo Sperati <matteo.sperati@dgsspa.com>
2025-03-27 18:12:56 +0100

    QA 6772
    microfix dovuta a refactor

--- src/test/java/it/pagopa/pn/cucumber/steps/pa/AsyncSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/pa/AsyncSteps.java
@@ -75,7 +75,7 @@ public class AsyncSteps {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         PaymentPositionModel paymentPositionModelSend = new PaymentPositionModel()
-                .iupd(String.format("%s-64c8e41bfec846e04%s-%d", organitationCode, iuv, System.currentTimeMillis()))
+                .iupd(String.format("%s-64c8e41bfec846e04%s-%d", organizationCode, iuv, System.currentTimeMillis()))
                 .type(PaymentPositionModel.TypeEnum.F)
                 .companyName("Automation")
                 .fullName(name)
