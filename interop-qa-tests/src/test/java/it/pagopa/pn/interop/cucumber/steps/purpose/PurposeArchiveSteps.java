b3e0f56743fc41cfcd5bbe594072e4d23e4cf0d6
Matteo Mancini <matteo.mancini@alten.it>
2025-04-23 17:51:16 +0200

    QA-7333: added PurposeArchiveSteps.java

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/purpose/PurposeArchiveSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/purpose/PurposeArchiveSteps.java
@@ -1,5 +1,35 @@
 package it.pagopa.pn.interop.cucumber.steps.purpose;
 
-public class PurposeArchiveSteps {
+import io.cucumber.java.en.When;
+import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
+import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
+import it.pagopa.pn.interop.cucumber.steps.common.PurposeCommonContext;
 
+import java.util.List;
+import java.util.UUID;
+
+public class PurposeArchiveSteps {
+    private final ClientTokenConfigurator clientTokenConfigurator;
+    private final SharedStepsContext sharedStepsContext;
+    private final PurposeCommonContext purposeCommonContext;
+
+    public PurposeArchiveSteps(ClientTokenConfigurator clientTokenConfigurator, SharedStepsContext sharedStepsContext) {
+        this.clientTokenConfigurator = clientTokenConfigurator;
+        this.sharedStepsContext = sharedStepsContext;
+        this.purposeCommonContext = sharedStepsContext.getPurposeCommonContext();
+    }
+
+    @When("l'utente archivia quella finalità in stato {string}")
+    public void userArchivePurpose(String state) {
+        String versionId = List.of("WAITING_FOR_APPROVAL", "REJECTED").contains(state)
+                ? purposeCommonContext.getWaitingForApprovalVersionId()
+                : purposeCommonContext.getVersionId();
+
+        sharedStepsContext.getHttpCallExecutor().performCall(
+                () -> clientTokenConfigurator.getPurposeApiClient().archivePurposeVersion(
+                        UUID.fromString(purposeCommonContext.getPurposeId()),
+                        UUID.fromString(versionId)
+                )
+        );
+    }
 }
