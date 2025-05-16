e9048ddd5c759c37dc1af1f6c96ddeed6203de87
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-05-16 11:11:41 +0200

    QA-7257: edit exec mod

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/AgreementApprovalPolicyFFONTest.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/AgreementApprovalPolicyFFONTest.java
@@ -1,13 +1,10 @@
 package it.pagopa.pn.interop.cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME;
-
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeTags;
 
 // Classe runner di test per la modifica del campo agreementApprovalPolicy dove il feature flag è attivato
 @IncludeTags("app-edit-ff-on")
-@ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread") // FIXME 15/05/2025 per facilitare la diagnostica del problema che sta impedendo il completamento della Github Action. Rimuovere una volta risolto.
+//@ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread") // FIXME 15/05/2025 per facilitare la diagnostica del problema che sta impedendo il completamento della Github Action. Rimuovere una volta risolto.
 public class AgreementApprovalPolicyFFONTest extends AbstractAgreementApprovalPolicyTest{
 
 }
