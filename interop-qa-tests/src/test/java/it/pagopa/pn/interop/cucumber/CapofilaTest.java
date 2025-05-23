43692bc975b78e9ddc4d519f60083234f1d4a912
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-05-23 12:36:28 +0200

    QA-7748: modified CapofilaTest exec mode

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/CapofilaTest.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/CapofilaTest.java
@@ -1,8 +1,16 @@
 package it.pagopa.pn.interop.cucumber;
 
-import org.junit.platform.suite.api.*;
+import static io.cucumber.junit.platform.engine.Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -12,7 +20,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
                 "html:target/cucumber-report.html"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.interop.cucumber.steps"),
-        @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread"),
+        @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
 @ExcludeTags({"wait_for_fix"})
 @IncludeTags({"capofila"})
