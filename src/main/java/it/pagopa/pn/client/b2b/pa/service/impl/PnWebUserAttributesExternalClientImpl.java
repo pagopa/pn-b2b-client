de6f229642b30bb15f1c325f9e371905a65151c6
Matteo Sperati <matteo.sperati@dgsspa.com>
2025-05-29 11:59:42 +0200

    QA 7077
    Fix provvisoria per BffChannelType (metodo "Viene rimossa se presente la pec di piattaforma di XXX"

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnWebUserAttributesExternalClientImpl.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnWebUserAttributesExternalClientImpl.java
@@ -1,6 +1,5 @@
 package it.pagopa.pn.client.b2b.pa.service.impl;
 
-import it.pagopa.pn.client.b2b.pa.exception.IllegalConfigurationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,9 +13,9 @@ import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model
 import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.recipient.digitaladdresses.BffChannelType;
 import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.tos.privacy.BffConsent;
 import it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.model.external.bff.tos.privacy.BffTosPrivacyActionBody;
+import it.pagopa.pn.client.b2b.pa.exception.IllegalConfigurationException;
 import it.pagopa.pn.client.b2b.pa.exception.PnB2bException;
 import it.pagopa.pn.client.b2b.pa.service.IPnWebUserAttributesClient;
-import it.pagopa.pn.client.web.generated.openapi.clients.externalUserAttributes.addressBook.api.CourtesyApi;
 import it.pagopa.pn.client.web.generated.openapi.clients.externalUserAttributes.addressBook.model.*;
 import it.pagopa.pn.client.web.generated.openapi.clients.externalUserAttributes.consents.model.Consent;
 import it.pagopa.pn.client.web.generated.openapi.clients.externalUserAttributes.consents.model.ConsentAction;
@@ -90,7 +89,7 @@ public class PnWebUserAttributesExternalClientImpl implements IPnWebUserAttribut
                 new it.pagopa.pn.client.b2b.generated.openapi.clients.external.generate.api.external.bff.tos.ApiClient(restTemplate);
         newApiClient.setBasePath(basePath);
         newApiClient.addDefaultHeader("user-agent", userAgent);
-        newApiClient.addDefaultHeader("Authorization","Bearer " + bearerToken);
+        newApiClient.addDefaultHeader("Authorization", "Bearer " + bearerToken);
         return newApiClient;
     }
 
@@ -98,7 +97,7 @@ public class PnWebUserAttributesExternalClientImpl implements IPnWebUserAttribut
         ApiClient newApiClient = new ApiClient(restTemplate);
         newApiClient.setBasePath(basePath);
         newApiClient.addDefaultHeader("user-agent", userAgent);
-        newApiClient.addDefaultHeader("Authorization","Bearer " + bearerToken);
+        newApiClient.addDefaultHeader("Authorization", "Bearer " + bearerToken);
         return newApiClient;
     }
 
@@ -205,13 +204,13 @@ public class PnWebUserAttributesExternalClientImpl implements IPnWebUserAttribut
 
         UserAddresses userAddresses = new UserAddresses();
         addressesApi.getAddressesV1()
-            .forEach(item -> {
-                if ("LEGAL".equals(item.getAddressType())) {
-                    legal.add(deepCopy(item, LegalAndUnverifiedDigitalAddress.class));
-                }else {
-                    courtesy.add(deepCopy(item, CourtesyDigitalAddress.class));
-                }
-            });
+                .forEach(item -> {
+                    if ("LEGAL".equals(item.getAddressType())) {
+                        legal.add(deepCopy(item, LegalAndUnverifiedDigitalAddress.class));
+                    } else {
+                        courtesy.add(deepCopy(item, CourtesyDigitalAddress.class));
+                    }
+                });
         userAddresses.legal(legal);
         userAddresses.courtesy(courtesy);
         return userAddresses;
@@ -232,7 +231,7 @@ public class PnWebUserAttributesExternalClientImpl implements IPnWebUserAttribut
 
     public void postRecipientLegalAddress(String senderId, LegalChannelType channelType, AddressVerification addressVerification) throws RestClientException {
         BffAddressVerificationRequest bffAddressVerificationRequest = new BffAddressVerificationRequest().requestId(addressVerification.getRequestId())
-                        .verificationCode(addressVerification.getVerificationCode()).value(addressVerification.getValue());
+                .verificationCode(addressVerification.getVerificationCode()).value(addressVerification.getValue());
         addressesApi.createOrUpdateAddressV1(BffAddressType.LEGAL, senderId, BffChannelType.fromValue(channelType.getValue()), bffAddressVerificationRequest);
     }
 
@@ -244,23 +243,34 @@ public class PnWebUserAttributesExternalClientImpl implements IPnWebUserAttribut
         return addressesApi.getAddressesV1().stream()
                 .filter(item -> "COURTESY".equals(item.getAddressType()))
                 .map(item -> deepCopy(item, CourtesyDigitalAddress.class))
-                .toList();    }
+                .toList();
+    }
 
     public void postRecipientCourtesyAddress(String senderId, CourtesyChannelType channelType, AddressVerification addressVerification) throws RestClientException {
         BffAddressVerificationRequest bffAddressVerificationRequest = new BffAddressVerificationRequest().requestId(addressVerification.getRequestId())
                 .verificationCode(addressVerification.getVerificationCode()).value(addressVerification.getValue());
-        addressesApi.createOrUpdateAddressV1(BffAddressType.COURTESY, senderId, BffChannelType.fromValue(channelType.getValue()), bffAddressVerificationRequest);    }
+        addressesApi.createOrUpdateAddressV1(BffAddressType.COURTESY, senderId, BffChannelType.fromValue(channelType.getValue()), bffAddressVerificationRequest);
+    }
 
-    private <T> T deepCopy( Object obj, Class<T> toClass) {
+    private <T> T deepCopy(Object obj, Class<T> toClass) {
         ObjectMapper objMapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .build();
         try {
-            String json = objMapper.writeValueAsString( obj );
-            return objMapper.readValue( json, toClass );
+            String json = objMapper.writeValueAsString(obj);
+            json = editChannelType(json);
+            return objMapper.readValue(json, toClass);
         } catch (JsonProcessingException exc) {
             throw new PnB2bException(exc.getMessage());
         }
+    }
+
+    /**
+     * Attualmente vi è discrepanza tra l'enum BffChannelType e LegalChannelType
+     * TODO IMPORTANTE: in attesa che vengano resi compatibili, dobbiamo ricorrere a questo metodo per evitare il fail dei test
+     */
+    private String editChannelType(String json) {
+        return json.replace("\"channelType\":\"SERCQ_SEND\"", "\"channelType\":\"SERCQ\"");
     }
 }
