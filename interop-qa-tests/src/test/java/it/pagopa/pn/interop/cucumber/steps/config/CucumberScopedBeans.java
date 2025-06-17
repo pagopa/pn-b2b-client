f5ac6f52f635f4bfd74171f13d66aa8c4f3391cf
gveldorale <giuseppe.veldorale@alten.it>
2025-06-17 12:02:40 +0200

    QA-7170: java doc

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/config/CucumberScopedBeans.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/config/CucumberScopedBeans.java
@@ -8,6 +8,20 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
+/**
+ * Definisce e registra un bean con scope di scenario.
+ * <p>
+ * Questo metodo è usato per creare istanze delle classi di step che NON contengono
+ * direttamente metodi glue (cioè metodi annotati con {@code @Given}, {@code @When},
+ * {@code @Then}, {@code @And}, ecc.), ma che servono come supporto ad altri step (@link SharedStep).
+ * <p>
+ * I bean registrati qui vengono gestiti da Spring con ciclo di vita limitato allo
+ * scenario Cucumber in esecuzione, grazie all'annotazione {@code @ScenarioScope}.
+ *
+ * @param sharedStepsContext oggetto condiviso tra gli step per mantenere lo stato
+ * @param clientTokenConfigurator componente per configurare il token di autenticazione
+ * @return una nuova istanza di {@link EserviceSteps} gestita da Spring
+ */
 public class CucumberScopedBeans {
 
     @Bean(name = "eserviceSteps")
