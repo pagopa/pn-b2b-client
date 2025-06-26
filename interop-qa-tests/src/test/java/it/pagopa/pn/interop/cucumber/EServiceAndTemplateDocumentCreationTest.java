819dc67eb728765d60933d63ee5ce0cdb5a03f6f
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-06-26 10:23:42 +0200

    QA-8271: introduced document creation test suite

---
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/EServiceAndTemplateDocumentCreationTest.java
@@ -0,0 +1,30 @@
+package it.pagopa.pn.interop.cucumber;
+
+import static io.cucumber.junit.platform.engine.Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+import it.pagopa.pn.interop.cucumber.e_service_template.version.document.EServiceTemplateVersionDocumentCreateTest;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+/* Testing https://pagopa.atlassian.net/browse/QA-8271 */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("it/pagopa/pn/cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
+                "html:target/cucumber-report.html"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.interop.cucumber.steps"),
+        @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread"),
+})
+@IncludeTags({
+    EServiceTemplateVersionDocumentCreateTest.TAG,
+    "document_upload"})
+public class EServiceAndTemplateDocumentCreationTest {
+}
