9cab3e5965c054c8bf6ed45fd4775dbc8bb208d3
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-06-30 17:40:40 +0200

    QA-7170: updated AgreementsListRequest

--- interop-qa-tests/src/main/java/it/pagopa/interop/agreement/service/IM2MAgreementClient.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/agreement/service/IM2MAgreementClient.java
@@ -21,6 +21,7 @@ public interface IM2MAgreementClient extends SettableBearerToken {
         private List<UUID> eservicesIds;
         private List<UUID> producersIds;
         private List<UUID> consumersIds;
+        private List<UUID> descriptorsIds;
         private List<AgreementState> states;
         private Boolean showOnlyUpgradeable;
     }
