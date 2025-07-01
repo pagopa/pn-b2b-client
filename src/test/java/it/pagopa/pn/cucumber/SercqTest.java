a2b85959281c4863f097fd13effe5d79c508e614
Tullio Ranieri <ranieritullio@gmail.com>
2025-07-01 12:02:53 +0200

    QA 15363 suite tag added

--- src/test/java/it/pagopa/pn/cucumber/SercqTest.java
+++ src/test/java/it/pagopa/pn/cucumber/SercqTest.java
@@ -13,7 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore"})
-//@IncludeTags({"sercq","sercqTos"})
-@IncludeTags({"sercqTos"})
+@IncludeTags({"sercq","sercqTos"})
 public class SercqTest {
 }
