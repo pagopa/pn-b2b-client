3dd206bea950fcef43fc42d9495f245765fd842f
Matteo Mancini <matteo.mancini@alten.it>
2025-07-14 11:45:17 +0200

    QA-8340: updated invokeRequest object with arn

--- src/test/java/it/pagopa/pn/cucumber/utils/LambdaInvoker.java
+++ src/test/java/it/pagopa/pn/cucumber/utils/LambdaInvoker.java
@@ -2,6 +2,7 @@ package it.pagopa.pn.cucumber.utils;
 
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.lambda.model.InvokeResponse;
@@ -16,14 +17,14 @@ public class LambdaInvoker {
     private LambdaClient getLambdaClient() {
         // Lazy init per evitare carico anticipato
         if (lambdaClient == null) {
-            lambdaClient = LambdaClient.create();
+            lambdaClient = LambdaClient.builder().region(Region.EU_SOUTH_1).build();
         }
         return lambdaClient;
     }
 
     public String invokeMyLambda(String functionName, String payload) {
         InvokeRequest request = InvokeRequest.builder()
-                .functionName(functionName)
+                .functionName("arn:aws:lambda:eu-south-1:151559006927:function:pn-ioAuthorizerLambda")
                 .payload(SdkBytes.fromString(payload, StandardCharsets.UTF_8))
                 .build();
 
