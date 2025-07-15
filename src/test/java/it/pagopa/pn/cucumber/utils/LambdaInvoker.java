81f8c8433cb952908803423139ed17ed3b501a83
gveldorale <giuseppe.veldorale@alten.it>
2025-07-15 12:21:39 +0200

    QA-8340: aggiunto supporto al profilo

--- src/test/java/it/pagopa/pn/cucumber/utils/LambdaInvoker.java
+++ src/test/java/it/pagopa/pn/cucumber/utils/LambdaInvoker.java
@@ -1,6 +1,8 @@
 package it.pagopa.pn.cucumber.utils;
 
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
@@ -15,16 +17,18 @@ public class LambdaInvoker {
     private LambdaClient lambdaClient;
 
     private LambdaClient getLambdaClient() {
-        // Lazy init per evitare carico anticipato
         if (lambdaClient == null) {
-            lambdaClient = LambdaClient.builder().region(Region.EU_SOUTH_1).build();
+            lambdaClient = LambdaClient.builder()
+                    //.credentialsProvider(ProfileCredentialsProvider.create("ROLE_dev_core"))
+                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .region(Region.EU_SOUTH_1).build();
         }
         return lambdaClient;
     }
 
     public String invokeMyLambda(String functionName, String payload) {
         InvokeRequest request = InvokeRequest.builder()
-                .functionName("arn:aws:lambda:eu-south-1:151559006927:function:pn-ioAuthorizerLambda")
+                .functionName(functionName)
                 .payload(SdkBytes.fromString(payload, StandardCharsets.UTF_8))
                 .build();
 
