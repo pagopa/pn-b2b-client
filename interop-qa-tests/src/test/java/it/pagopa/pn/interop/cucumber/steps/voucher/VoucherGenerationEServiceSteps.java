2480465363f5a1025d0b7f87db198f2bd10ae3fe
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-07-21 14:25:16 +0200

    QA-8665: updated with correct error msg

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/voucher/VoucherGenerationEServiceSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/voucher/VoucherGenerationEServiceSteps.java
@@ -8,12 +8,11 @@ import io.cucumber.java.en.Then;
 import it.pagopa.interop.authorization.service.identity.IdentityService;
 import it.pagopa.interop.common.IHttpExecutor;
 import it.pagopa.interop.generated.openapi.clients.bff.model.EServiceDescriptorState;
-import it.pagopa.interop.utils.HttpCallExecutor;
 import it.pagopa.interop.utils.InteropAPIErrorResponse;
 import it.pagopa.interop.utils.InteropAPIErrorResponse.InteropAPIError;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
-import it.pagopa.pn.interop.cucumber.steps.datapreparationservice.BFFDataPreparationService;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
+import it.pagopa.pn.interop.cucumber.steps.datapreparationservice.BFFDataPreparationService;
 import java.util.List;
 import java.util.UUID;
 
@@ -90,13 +89,14 @@ public class VoucherGenerationEServiceSteps {
         InteropAPIErrorResponse possibleResponse2 = InteropAPIErrorResponse.builder()
             .errors(List.of(
                 InteropAPIError.builder()
-                    .code("9999")
+                    .code("007-9999")
                     .detail(
                         "Validation error: Invalid literal value, expected \"client_credentials\" at \"grant_type\"")
                     .build()))
             .status("400")
             .title("Bad request")
             .type("about:blank")
+            .detail("Incorrect value for body")
             .build();
         InteropAPIErrorResponse originalResponse = new ObjectMapper().convertValue(
             httpCallExecutor.getResponse(),
