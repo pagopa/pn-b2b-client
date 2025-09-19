f6f04a28560dacf619ca438494cd487e8096d10d
Matteo Mancini <matteo.mancini@alten.it>
2025-09-19 12:10:02 +0200

    QA-9168: marked EServiceTemplateStepContext with scope PROTOTYPE

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
@@ -15,8 +15,8 @@ import it.pagopa.interop.generated.openapi.clients.bff.model.UpdateEServiceTempl
 import it.pagopa.interop.generated.openapi.clients.bff.model.VersionSeedForEServiceTemplateCreation;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
+import it.pagopa.pn.interop.cucumber.steps.common.EServiceTemplateInfo;
 import it.pagopa.pn.interop.cucumber.steps.datapreparationservice.BFFDataPreparationService;
-import it.pagopa.pn.interop.cucumber.steps.e_service_template.shared.EServiceTemplateStepContext.EServiceTemplateInfo;
 import it.pagopa.pn.interop.cucumber.steps.e_service_template.shared.EServiceTemplateTestAssistant;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
