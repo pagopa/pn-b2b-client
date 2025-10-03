1b9c181826b37951bcb58d659fefd08d096e1096
Matteo Mancini <matteo.mancini@pagopa.it>
2025-10-03 15:46:39 +0200

    QA-9280: renamed RegistroBeniProducts.java to RegistroBeniTest.java

--- pari/src/test/java/it/pagopa/pari/cucumber/RegistroBeniTest.java
+++ pari/src/test/java/it/pagopa/pari/cucumber/RegistroBeniTest.java
@@ -18,6 +18,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
         "html:target/cucumber-report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pari.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread")
-@IncludeTags({"rdbTest"})
-public class RegistroBeniProducts {
+@IncludeTags({"upload-csv"})
+public class RegistroBeniTest {
 }
