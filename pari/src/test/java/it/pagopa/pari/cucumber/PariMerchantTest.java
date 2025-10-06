e246efaf6a3594de3f0e4fe36e8ac07a25500aae
Matteo Mancini <matteo.mancini@pagopa.it>
2025-10-06 15:55:48 +0200

    QA-9280: updated PariMerchantTest.java

--- pari/src/test/java/it/pagopa/pari/cucumber/PariMerchantTest.java
+++ pari/src/test/java/it/pagopa/pari/cucumber/PariMerchantTest.java
@@ -6,7 +6,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("it/pagopa/pn/cucumber/registrobeni")
+@SelectClasspathResource("it/pagopa/pn/cucumber/esercenti")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
         "html:target/cucumber-report.html")
