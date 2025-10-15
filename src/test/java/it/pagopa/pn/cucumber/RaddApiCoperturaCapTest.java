17b9e089d3470d2831c39ed98ea47374f8445500
Tullio Ranieri <ranieritullio@gmail.com>
2025-10-15 15:08:33 +0200

    QA 9114 fix external url

--- src/test/java/it/pagopa/pn/cucumber/RaddApiCoperturaCapTest.java
+++ src/test/java/it/pagopa/pn/cucumber/RaddApiCoperturaCapTest.java
@@ -13,6 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore"})
-@IncludeTags({"raddCoperturaCap"})
+@IncludeTags({"capCoverageRadd"})
 public class RaddApiCoperturaCapTest {
 }
