9d3fd9afa85daa2165d297a201567c6e0cc7f44d
Tullio Ranieri <ranieritullio@gmail.com>
2025-10-24 14:27:19 +0200

    QA tag sercQ Fase 2

--- src/test/java/it/pagopa/pn/cucumber/SercqTest.java
+++ src/test/java/it/pagopa/pn/cucumber/SercqTest.java
@@ -13,7 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore"})
-@IncludeTags({"sercq","sercqTos","sercqF2"})
-//@IncludeTags({"sercqF2"})
+//@IncludeTags({"sercq","sercqTos","sercqF2"})
+@IncludeTags({"sercqF2"})
 public class SercqTest {
 }
