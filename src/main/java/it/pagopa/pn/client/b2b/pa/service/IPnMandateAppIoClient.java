4b4e733f63fc51312b6fa3e227a931e7cb28c23d
Matteo Sperati <matteo.sperati@dgsspa.com>
2025-10-28 15:13:19 +0100

    QA15755-log base path appIo after setting

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/IPnMandateAppIoClient.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/IPnMandateAppIoClient.java
@@ -3,10 +3,9 @@ package it.pagopa.pn.client.b2b.pa.service;
 import it.pagopa.pn.client.b2b.generated.openapi.clients.mandateIo.model.CIEValidationData;
 import it.pagopa.pn.client.b2b.generated.openapi.clients.mandateIo.model.MandateCreationRequest;
 import it.pagopa.pn.client.b2b.generated.openapi.clients.mandateIo.model.MandateCreationResponse;
-import it.pagopa.pn.client.b2b.pa.service.utils.SettableBearerToken;
 import org.springframework.web.client.RestClientException;
 
-public interface IPnMandateAppIoClient extends SettableBearerToken {
+public interface IPnMandateAppIoClient {
 
     MandateCreationResponse createIOMandate(String xPagopaCxTaxid, String xPagopaLollipopOriginalUrl, String xPagopaLollipopOriginalMethod, String xPagopaLollipopPublicKey, String xPagopaLollipopAssertionRef, String xPagopaLollipopAssertionType, String xPagopaLollipopAuthJwt, String xPagopaLollipopUserId, String signatureInput, String signature, MandateCreationRequest mandateCreationRequest) throws RestClientException;
 
--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnMandateAppIoClientImpl.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnMandateAppIoClientImpl.java
@@ -20,90 +20,26 @@ import org.springframework.web.client.RestTemplate;
 public class PnMandateAppIoClientImpl implements IPnMandateAppIoClient {
 
     private final RestTemplate restTemplate;
-    private String marioCucumberBearerToken;
-    private String marioGherkinBearerToken;
-    private String leonardoBearerToken;
-    private String gherkinSrlBearerToken;
-    private String cucumberSpaBearerToken;
-
-    private BearerTokenType bearerTokenSetted;
-
     private final String basePath;
     private final AppIoPnMandateCreateApi appIoMandateApi;
-
     private final String appIoApiKey;
 
     public PnMandateAppIoClientImpl(RestTemplate restTemplate,
                                     @Value("${pn.appio.externa.base-url.pagopa}") String basePath,
-                                    @Value("${pn.bearer-token.user1}") String marioCucumberBearerToken,
-                                    @Value("${pn.bearer-token.user2}") String marioGherkinBearerToken,
-                                    @Value("${pn.bearer-token.user3}") String leonardoBearerToken,
-                                    @Value("${pn.bearer-token.user4}") String galileoBearerToken,
-                                    @Value("${pn.bearer-token.user5}") String dinoBearerToken,
-                                    @Value("${pn.bearer-token.scaduto}") String userBearerTokenScaduto,
-                                    @Value("${pn.bearer-token.pg1}") String gherkinSrlBearerToken,
-                                    @Value("${pn.bearer-token-b2b.pg2}") String cucumberSpaBearerToken,
                                     @Value("${pn.external.appio.api-key}") String appIoApiKey) {
         this.restTemplate = restTemplate;
         this.basePath = basePath;
-        this.marioCucumberBearerToken = marioCucumberBearerToken;
-        this.marioGherkinBearerToken = marioGherkinBearerToken;
-        this.leonardoBearerToken = leonardoBearerToken;
-        this.gherkinSrlBearerToken = gherkinSrlBearerToken;
-        this.cucumberSpaBearerToken = cucumberSpaBearerToken;
         this.appIoApiKey = appIoApiKey;
-        this.bearerTokenSetted = BearerTokenType.PG_1;
-        this.appIoMandateApi = new AppIoPnMandateCreateApi(newApiClient(restTemplate, basePath, marioGherkinBearerToken));
+        this.appIoMandateApi = new AppIoPnMandateCreateApi(newApiClient(restTemplate, basePath));
     }
 
 
-    private ApiClient newApiClient(RestTemplate restTemplate, String basePath, String bearerToken) {
+    private ApiClient newApiClient(RestTemplate restTemplate, String basePath) {
+        log.info("APP IO BASEPATH: " + basePath);
         ApiClient newApiClient = new ApiClient(restTemplate);
         newApiClient.setBasePath(basePath);
-//        newApiClient.addDefaultHeader("user-agent", userAgent);
-//        newApiClient.addDefaultHeader("Authorization", "Bearer " + bearerToken);
         newApiClient.addDefaultHeader("x-api-key", appIoApiKey);
         return newApiClient;
-    }
-
-    @Override
-    public boolean setBearerToken(BearerTokenType bearerToken) {
-        boolean beenSet = false;
-        switch (bearerToken) {
-            case USER_1 -> {
-                this.appIoMandateApi.setApiClient(newApiClient(restTemplate, basePath, marioCucumberBearerToken));
-                this.bearerTokenSetted = BearerTokenType.USER_1;
-                beenSet = true;
-            }
-            case USER_2 -> {
-                this.appIoMandateApi.setApiClient(newApiClient(restTemplate, basePath, marioGherkinBearerToken));
-                this.bearerTokenSetted = BearerTokenType.USER_2;
-                beenSet = true;
-            }
-            case USER_3 -> {
-                this.appIoMandateApi.setApiClient(newApiClient(restTemplate, basePath, leonardoBearerToken));
-                this.bearerTokenSetted = BearerTokenType.USER_3;
-                beenSet = true;
-            }
-            case PG_1 -> {
-                this.appIoMandateApi.setApiClient(newApiClient(restTemplate, basePath, gherkinSrlBearerToken));
-                this.bearerTokenSetted = BearerTokenType.PG_1;
-                beenSet = true;
-            }
-            case PG_2 -> {
-                this.appIoMandateApi.setApiClient(newApiClient(restTemplate, basePath, cucumberSpaBearerToken));
-                this.bearerTokenSetted = BearerTokenType.PG_2;
-                beenSet = true;
-            }
-            default -> throw new IllegalStateException("Unexpected value: " + bearerToken);
-        }
-        log.info("APP IO BASEPATH: " + basePath);
-        return beenSet;
-    }
-
-    @Override
-    public BearerTokenType getBearerTokenSetted() {
-        return null;
     }
 
     @Override
