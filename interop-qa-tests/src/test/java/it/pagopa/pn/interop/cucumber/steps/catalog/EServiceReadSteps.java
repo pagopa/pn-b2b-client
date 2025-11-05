40594ae68ee952834249412c282f2e10f2e326e1
Vincenzo Massaro <vincenzo.massaro@alten.it>
2025-11-05 14:45:40 +0100

    Corretto EServiceReadSteps#requireEServiceRead e ripristinato
    Il metodo EServiceReadSteps#requireEServiceRead non deve effettuare polling sullo status code 200, perché usato anche da test in cui il risultato atteso è un fallimento

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/EServiceReadSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/EServiceReadSteps.java
@@ -1,7 +1,6 @@
 package it.pagopa.pn.interop.cucumber.steps.catalog;
 
 import io.cucumber.java.en.When;
-import it.pagopa.interop.authorization.service.utils.PollingService;
 import it.pagopa.interop.common.IHttpExecutor;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
@@ -9,24 +8,18 @@ import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 public class EServiceReadSteps {
     private final ClientTokenConfigurator clientTokenConfigurator;
     private final SharedStepsContext sharedStepsContext;
-    private final PollingService pollingService;
 
     public EServiceReadSteps(ClientTokenConfigurator clientTokenConfigurator, SharedStepsContext sharedStepsContext) {
         this.clientTokenConfigurator = clientTokenConfigurator;
         this.sharedStepsContext = sharedStepsContext;
-        this.pollingService = sharedStepsContext.getPollingService();
     }
 
     @When("l'utente richiede la lettura di quell'e-service")
     public void requireEServiceRead() {
         clientTokenConfigurator.setBearerToken(sharedStepsContext.getUserToken());
         IHttpExecutor httpCallExecutor = sharedStepsContext.getHttpCallExecutor();
-        pollingService.makePolling(
-            () -> httpCallExecutor.performCall(
-                () -> clientTokenConfigurator.getProducerClient().getProducerEServiceDetails(
-                        sharedStepsContext.getEServicesCommonContext().getEserviceId())),
-            res -> httpCallExecutor.getResponseStatus().is2xxSuccessful(),
-            "Lettura e-service non riuscita"
-        );
+        httpCallExecutor.performCall(
+            () -> clientTokenConfigurator.getProducerClient().getProducerEServiceDetails(
+                    sharedStepsContext.getEServicesCommonContext().getEserviceId()));
     }
 }
