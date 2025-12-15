9d34aafa6289ce87354be854afe97bf1977ad487
gveldorale <giuseppe.veldorale@alten.it>
2025-12-16 00:59:19 +0100

    QA-8205-fix(FileInfoRegistry): fix constructor

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/file/registry/FileInfoRegistry.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/file/registry/FileInfoRegistry.java
@@ -8,7 +8,6 @@ import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.file.model.
 import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.file.validator.FileValidator;
 import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.client.model.S3BucketInfoBuilder;
 import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.utils.TokenResolver;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
 import java.util.Set;
@@ -16,40 +15,39 @@ import java.util.Set;
 import static it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.client.model.BucketRole.*;
 import static it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.enums.InteropFile.*;
 
-@RequiredArgsConstructor
+
 public class FileInfoRegistry {
 
-    private final TokenResolver tokenResolver;
-    private final String documentBucketBase;
-    private final String documentWormBucketBase;
-    private final String eventBucketBase;
-    private final String eventWormBucketBase;
+    private final Map<InteropFile, FileInfo> registry;
 
-    private final Map<InteropFile, FileInfo> registry = Map.of(
-            RISK_ANALYSIS_DOC, new FileInfo(
-                    RISK_ANALYSIS_DOC,
-                    new FileValidator(tokenResolver, ListFileTokenSource.of("Analisi del rischio"), null),
-                    Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"risk-analysis/:riskAnalysisId").build(), FilenameFormat.PDF_DOC),
-                           new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
-            ),
+    public FileInfoRegistry(TokenResolver tokenResolver, String documentBucketBase, String documentWormBucketBase, String eventBucketBase, String eventWormBucketBase) {
 
-            AGREEMENT_CONTRACT_DOC, new FileInfo(
-                    AGREEMENT_CONTRACT_DOC,
-                    new FileValidator(tokenResolver, ListFileTokenSource.of("richiesta di fruizione", ":agreementId"), null),
-                    Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"agreement/:agreementId").build(), FilenameFormat.PDF_DOC),
-                            new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
-            ),
+        this.registry = Map.of(
+                RISK_ANALYSIS_DOC, new FileInfo(
+                        RISK_ANALYSIS_DOC,
+                        new FileValidator(tokenResolver, ListFileTokenSource.of("Analisi del rischio"), null),
+                        Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"risk-analysis/:riskAnalysisId").build(), FilenameFormat.PDF_DOC),
+                                new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
+                ),
 
-            CONSUMER_DELEGATION_REQUEST_DOC, new FileInfo(
-                    CONSUMER_DELEGATION_REQUEST_DOC,
-                    new FileValidator(tokenResolver, ListFileTokenSource.of("richiesta di delega alla fruizione", ":agreementId"), null),
-                    Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"delegation/:consumerDelegationId").build(), FilenameFormat.PDF_DOC),
-                            new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
-            ),
+                AGREEMENT_CONTRACT_DOC, new FileInfo(
+                        AGREEMENT_CONTRACT_DOC,
+                        new FileValidator(tokenResolver, ListFileTokenSource.of("richiesta di fruizione", ":agreementId"), null),
+                        Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"agreement/:agreementId").build(), FilenameFormat.PDF_DOC),
+                                new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
+                ),
+
+                CONSUMER_DELEGATION_REQUEST_DOC, new FileInfo(
+                        CONSUMER_DELEGATION_REQUEST_DOC,
+                        new FileValidator(tokenResolver, ListFileTokenSource.of("richiesta di delega alla fruizione", ":agreementId"), null),
+                        Set.of(new FileLocation(STANDARD, S3BucketInfoBuilder.builder().fullPath(documentBucketBase+"delegation/:consumerDelegationId").build(), FilenameFormat.PDF_DOC),
+                                new FileLocation(WORM, S3BucketInfoBuilder.builder().fullPath(documentWormBucketBase).build(), FilenameFormat.PDF_SIGNED_DOC))
+                ),
 
 
-            CONSUMER_DELEGATION_REVOKED_DOC, null
-    );
+                CONSUMER_DELEGATION_REVOKED_DOC, null
+        );
+    }
 
     public FileInfo getFileInfo(InteropFile file) {
         FileInfo info = registry.get(file);
