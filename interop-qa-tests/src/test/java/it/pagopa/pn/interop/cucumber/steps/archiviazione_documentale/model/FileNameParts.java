d38c79dbaee3b81dc44525d3844a04e6b3def373
gveldorale <giuseppe.veldorale@alten.it>
2025-12-16 00:15:27 +0100

    QA-8205-refactor: delete deprecated

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/model/FileNameParts.java
+++
@@ -1,3 +0,0 @@
-package it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.model;
-
-public record FileNameParts(String timestamp, String baseName, String extension) {}
--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/model/ValidationResult.java
+++
@@ -1,6 +0,0 @@
-package it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.model;
-
-import java.util.List;
-
-public record ValidationResult(boolean valid, List<String> errors, String rawJson) {}
-
