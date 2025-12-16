206b2fdce24c5ab1bcf0884f773e17f85188d746
gveldorale <giuseppe.veldorale@alten.it>
2025-12-16 14:39:42 +0100

    QA-8205-feat(FileLocation): add resolve method

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/file/model/FileLocation.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/file/model/FileLocation.java
@@ -2,10 +2,34 @@ package it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.file.model
 
 import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.client.model.BucketRole;
 import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.client.model.BucketUrl;
+import it.pagopa.pn.interop.cucumber.steps.archiviazione_documentale.utils.TokenResolver;
 
 public record FileLocation(
         BucketRole bucketRole,
         BucketUrl bucketUrl,
         FilenameFormat filenameFormat
 ) {
+
+    public FileLocation resolve(TokenResolver resolver) {
+        return new FileLocation(
+                bucketRole,
+                new BucketUrl(
+                        bucketUrl.base(),
+                        resolvePath(bucketUrl.prefix(), resolver),
+                        resolvePath(bucketUrl.key(), resolver)
+                ),
+                filenameFormat
+        );
+    }
+
+    private static String resolvePath(String path, TokenResolver resolver) {
+        if (path == null || path.isBlank()) {
+            return path;
+        }
+
+        return java.util.Arrays.stream(path.split("/"))
+                .map(resolver::resolve)
+                .collect(java.util.stream.Collectors.joining("/"));
+    }
 }
+
