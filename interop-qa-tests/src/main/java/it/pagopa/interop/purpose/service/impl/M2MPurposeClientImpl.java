2d57d8fb6d1e254018d9e768764424c41ec0d23d
gveldorale <giuseppe.veldorale@alten.it>
2026-02-09 18:27:04 +0100

    QA-12764 chore: adeguamenti alla nuova OpenApi per APIv2

--- interop-qa-tests/src/main/java/it/pagopa/interop/purpose/service/impl/M2MPurposeClientImpl.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/purpose/service/impl/M2MPurposeClientImpl.java
@@ -3,22 +3,14 @@ package it.pagopa.interop.purpose.service.impl;
 import it.pagopa.interop.conf.InteropClientConfigs;
 import it.pagopa.interop.generated.openapi.clients.m2mGateway.ApiClient;
 import it.pagopa.interop.generated.openapi.clients.m2mGateway.api.PurposesApi;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.Agreement;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.DelegationRef;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.FileDownloadMultipart;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.Purpose;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeDraftUpdateSeed;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeVersion;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeVersionSeed;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeVersions;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.Purposes;
-import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.ReversePurposeDraftUpdateSeed;
+import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.*;
 import it.pagopa.interop.purpose.service.IM2MPurposeClient;
-import java.util.UUID;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -131,7 +123,7 @@ public class M2MPurposeClientImpl implements IM2MPurposeClient {
     public Purpose patchPurpose(UUID purposeId, PurposePatchRequest body) {
         return purposesApi.updateDraftPurpose(
             purposeId,
-            new PurposeDraftUpdateSeed()
+                new UpdateDraftPurposeRequest()
                 .title(body.getTitle())
                 .description(body.getDescription())
                 .riskAnalysisForm(body.getRiskAnalysisForm())
