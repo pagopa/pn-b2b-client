8660059d2f68db75ad6b0ac256ca4a9b1abe1475
mmancini95 <matteo.mancini@pagopa.it>
2026-02-10 11:59:22 +0100

    QA-12059: feature: removed the used of DelayerCsvUtils since no longer needed and updated the logic to provide the deliveryWeek

--- src/test/java/it/pagopa/pn/cucumber/steps/delayer/utils/DelayerCsvUtils.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/delayer/utils/DelayerCsvUtils.java
@@ -16,68 +16,31 @@ import java.nio.file.StandardOpenOption;
 
 public class DelayerCsvUtils {
 
-    public void replaceCsvContent(Path path, String placeholder, String replaceWith) {
-        try {
-            String content = Files.readString(path, StandardCharsets.UTF_8);
-            content = content.replace(placeholder, replaceWith);
-            Files.writeString(path, content, StandardCharsets.UTF_8);
-            uploadCsvToS3(content, "", "");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-//    public void generateCsvContent(String iun) {
-//        String header = String.join(";",
-//                "requestId", "notificationSentAt", "prepareRequestDate",
-//                "productType", "senderPaId", "province",
-//                "cap", "attempt", "iun", "unifiedDeliveryDriver"
-//        );
-//        String row = String.join(";",
-//                "tcNotificationCancelled_1", "2026-01-19T00:00:00Z", "2026-01-19T00:00:00Z",
-//                "AR", "unknow", "NA", "80124",
-//                "0", iun, "Fulmine"
-//        );
-//        String csvContent = header + "\n" + row + "\n";
+//    public void replaceCsvContent(Path path, String placeholder, String replaceWith) {
 //        try {
-//            writeCsvToLocal(csvContent);
+//            String content = Files.readString(path, StandardCharsets.UTF_8);
+//            content = content.replace(placeholder, replaceWith);
+//            Files.writeString(path, content, StandardCharsets.UTF_8);
+//            uploadCsvToS3(content, "", path.getFileName().toString());
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
-//        uploadCsvToS3(csvContent, "", "");
 //    }
-
-//    public Path writeCsvToLocal(String csvContent) throws IOException {
-//        Path outputPath = Paths.get(
-//                "src", "test", "resources", "it",
-//                "pagopa", "pn", "cucumber", "workflowNotifica", "workflowAnalogico",
-//                "delayer", "csv", "notificationCancelled.csv");
-//        Files.createDirectories(outputPath.getParent());
-//        Files.writeString(
-//                outputPath,
-//                csvContent,
-//                StandardCharsets.UTF_8,
-//                StandardOpenOption.CREATE,
-//                StandardOpenOption.TRUNCATE_EXISTING
-//        );
 //
-//        return outputPath;
+//    public void uploadCsvToS3(String csvContent, String bucket, String key) {
+//        S3Client s3Client = S3Client.builder()
+//                .region(Region.EU_SOUTH_1)
+//                .credentialsProvider(ProfileCredentialsProvider.create("ROLE_test_core"))
+//                .build();
+//
+//        s3Client.putObject(
+//                PutObjectRequest.builder()
+//                        .bucket("")
+//                        .key(key)
+//                        .contentType("text/csv")
+//                        .serverSideEncryption(ServerSideEncryption.AWS_KMS)
+//                        .build(),
+//                RequestBody.fromString(csvContent, StandardCharsets.UTF_8)
+//        );
 //    }
-
-    public void uploadCsvToS3(String csvContent, String bucket, String key) {
-        S3Client s3Client = S3Client.builder()
-                .region(Region.EU_SOUTH_1)
-                .credentialsProvider(ProfileCredentialsProvider.create("prova-test"))
-                .build();
-
-        s3Client.putObject(
-                PutObjectRequest.builder()
-                        .bucket("")
-                        .key("notificationCancelled.csv")
-                        .contentType("text/csv")
-                        .serverSideEncryption(ServerSideEncryption.AWS_KMS)
-                        .build(),
-                RequestBody.fromString(csvContent, StandardCharsets.UTF_8)
-        );
-    }
 }
