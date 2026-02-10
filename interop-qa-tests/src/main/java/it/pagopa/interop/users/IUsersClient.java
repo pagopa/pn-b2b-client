4dc1029c736b7e0daeba64eecac5e957cc9977f5
99niccoloCacace <niccolo.cacace@alten.it>
2026-02-10 16:29:50 +0100

    QA-11072 feat: creazione UsersClient

---
+++ interop-qa-tests/src/main/java/it/pagopa/interop/users/IUsersClient.java
@@ -0,0 +1,13 @@
+package it.pagopa.interop.users;
+
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.User;
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.Users;
+import java.util.List;
+import java.util.UUID;
+
+public interface IUsersClient {
+
+	User getUser(UUID userId);
+
+	Users getUsers(Integer limit, Integer offset, List<String> roles);
+}
