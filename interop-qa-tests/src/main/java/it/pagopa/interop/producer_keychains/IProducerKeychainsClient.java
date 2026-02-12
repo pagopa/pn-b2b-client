bfa894732e5b3d3b4194244439a2b9468d4c442a
99niccoloCacace <niccolo.cacace@alten.it>
2026-02-12 18:07:11 +0100

    QA-11072 feat: creazione ProducerKeychainsClient

---
+++ interop-qa-tests/src/main/java/it/pagopa/interop/producer_keychains/IProducerKeychainsClient.java
@@ -0,0 +1,13 @@
+package it.pagopa.interop.producer_keychains;
+
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.KeySeed;
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.ProducerKey;
+
+import java.util.UUID;
+
+public interface IProducerKeychainsClient {
+
+    ProducerKey createProducerKeychainKey(UUID keychainId, KeySeed keySeed);
+
+    void deleteProducerKeychainKeyById(UUID keychainId, String keyId);
+}
