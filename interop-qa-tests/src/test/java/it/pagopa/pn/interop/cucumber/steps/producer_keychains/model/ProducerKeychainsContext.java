93a5d5c20735e91d1ff68105a647347325910ec2
gveldorale <giuseppe.veldorale@alten.it>
2026-02-16 19:56:54 +0100

    QA-12314 fix(ProducerKeychainsContext): Scope.PROTOTYPE -> ScenarioScope

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/producer_keychains/model/ProducerKeychainsContext.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/producer_keychains/model/ProducerKeychainsContext.java
@@ -1,16 +1,17 @@
 package it.pagopa.pn.interop.cucumber.steps.producer_keychains.model;
 
 import java.util.UUID;
+import io.cucumber.spring.ScenarioScope;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@ScenarioScope
 public class ProducerKeychainsContext {
     private UUID producerKeychainId;
 }
