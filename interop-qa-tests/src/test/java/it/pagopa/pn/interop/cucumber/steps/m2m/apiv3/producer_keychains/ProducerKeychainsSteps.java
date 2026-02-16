21ee7e51935df909ba08f7a4a310986d286ec1fc
99niccoloCacace <niccolo.cacace@alten.it>
2026-02-16 16:11:48 +0100

    QA-11072 feat: Scenario[M2M_V3_GET_PRODUCER_KEYCHAINS_USERS_VERIFICATION]

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/apiv3/producer_keychains/ProducerKeychainsSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/apiv3/producer_keychains/ProducerKeychainsSteps.java
@@ -1,15 +1,21 @@
 package it.pagopa.pn.interop.cucumber.steps.m2m.apiv3.producer_keychains;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 import it.pagopa.interop.common.IHttpExecutor;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.LinkUser;
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.User;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.Users;
 import it.pagopa.interop.producer_keychains.service.M2MProducerKeychainsClient;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import it.pagopa.pn.interop.cucumber.steps.producer_keychains.model.ProducerKeychainsContext;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import it.pagopa.pn.interop.cucumber.steps.selfcare.model.TenantContext;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 
@@ -19,11 +25,13 @@ public class ProducerKeychainsSteps {
     private final IHttpExecutor httpCallExecutor;
     private final SharedStepsContext sharedStepsContext;
     private final ProducerKeychainsContext producerKeychainsContext;
+    private final TenantContext tenantContext;
 
-    public ProducerKeychainsSteps(M2MProducerKeychainsClient producerKeychainsClient, SharedStepsContext sharedStepsContext, ProducerKeychainsContext producerKeychainsContext) {
+    public ProducerKeychainsSteps(M2MProducerKeychainsClient producerKeychainsClient, SharedStepsContext sharedStepsContext, ProducerKeychainsContext producerKeychainsContext, TenantContext tenantContext) {
 
         this.producerKeychainsClient = producerKeychainsClient;
         this.sharedStepsContext = sharedStepsContext;
+        this.tenantContext = tenantContext;
         this.producerKeychainsContext = producerKeychainsContext;
         this.httpCallExecutor = sharedStepsContext.getHttpCallExecutor();
         this.producerKeychainsClient.setHttpCallExecutor(this.httpCallExecutor);
@@ -31,6 +39,7 @@ public class ProducerKeychainsSteps {
     }
 
     @And("l'utente associa l'utenza con userId {string} alla producer keychain {string}")
+    @And("esiste un utente con id {string} associato alla keychain creata {string}")
     public void createProducerKeychainUserAssociation(String userId, String producerKeychainId) {
         UUID userIdValue = parseNullableUuid(userId);
         UUID producerKeychainValue = resolveProducerKeychainId(producerKeychainId);
@@ -47,7 +56,7 @@ public class ProducerKeychainsSteps {
         }
     }
 
-    @And("l'utente viene invocata l'API di recupero utenze associate alla producer keychain {string} con limit {string} offset {string}")
+    @And("viene invocata l'API di recupero utenze associate alla producer keychain {string} con limit {string} offset {string}")
     public void getProducerKeychainUsers(String producerKeychainId, String limit, String offset) {
         UUID producerKeychainValue = resolveProducerKeychainId(producerKeychainId);
         Integer limitValue = parseNullableInteger(limit);
@@ -61,6 +70,30 @@ public class ProducerKeychainsSteps {
         } catch (IllegalStateException e) {
             log.warn(e.getMessage());
         }
+    }
+
+    @When("si verifica che le utenze recuperate siano presenti nella lista di utenti appartenenti al tenant del chiamante")
+    public void verifyUsersPresence() {
+        List<User> m2mUsers = tenantContext.getM2mUsers();
+        List<it.pagopa.interop.generated.openapi.clients.bff.model.User> selfcareUsers = tenantContext.getSelfcareUsers();
+
+        Assertions.assertThat(m2mUsers)
+                .as("La lista utenti M2M non deve essere null")
+                .isNotNull();
+        Assertions.assertThat(selfcareUsers)
+                .as("La lista utenti Selfcare non deve essere null")
+                .isNotNull();
+
+        Set<UUID> m2mUserIds = m2mUsers.stream()
+                .map(User::getUserId)
+                .collect(Collectors.toSet());
+        Set<UUID> selfcareUserIds = selfcareUsers.stream()
+                .map(it.pagopa.interop.generated.openapi.clients.bff.model.User::getUserId)
+                .collect(Collectors.toSet());
+
+        Assertions.assertThat(selfcareUserIds)
+            .as("Gli userId restituiti da M2M devono essere contenuti in quelli di Selfcare")
+            .containsAll(m2mUserIds);
     }
 
     private UUID parseNullableUuid(String value) {
--- interop-qa-tests/src/test/resources/it/pagopa/pn/cucumber/m2mgateway/apiv3/producer-keychains.feature
+++ interop-qa-tests/src/test/resources/it/pagopa/pn/cucumber/m2mgateway/apiv3/producer-keychains.feature
@@ -24,9 +24,9 @@ Feature: Gestione dei producer keychains - API v3
       | 17a84b7b-dce6-4b8f-a1ae-85926c55f02e | PKC1               | m2m       | 403        |
 
   Scenario Outline: [M2M_V3_GET_PRODUCER_KEYCHAINS_USERS] Associazione utenze a producer keychain
-    Given l'utente è un "admin" di "PA1" con ruolo M2M m2m
+    Given l'utente è un "admin" di "PA1" con ruolo M2M m2m-admin
     And esiste un producer keychain con nome "PKC1" e con descrizione "DESC_PKC1"
-    When l'utente viene invocata l'API di recupero utenze associate alla producer keychain "<producerKeychainId>" con limit "<limit>" offset "<offset>"
+    When viene invocata l'API di recupero utenze associate alla producer keychain "<producerKeychainId>" con limit "<limit>" offset "<offset>"
     Then si ottiene status code <statusCode>
 
     #da implementare -> i 2 status 401
@@ -40,3 +40,13 @@ Feature: Gestione dei producer keychains - API v3
       | PKCreata           | 10    | null   | 400        |
       | PKCreata           | 10    | -1     | 400        |
       | PKCNonEsistente    | 10    | 0      | 404        |
+
+  Scenario: [M2M_V3_GET_PRODUCER_KEYCHAINS_USERS_VERIFICATION] Verifica che gli utenti restituiti, associati al producer keychain specificato, appartengano al tenant del richiedente
+    Given l'utente è un "admin" di "PA1" con ruolo M2M m2m-admin
+    And esiste un producer keychain con nome "PKC1" e con descrizione "DESC_PKC1"
+    And esiste un utente con id "c7dc1a86-31f6-4fe9-89cd-184201e29d75" associato alla keychain creata "PKCreata"
+    When viene invocata l'API di recupero utenze associate alla producer keychain "PKCreata" con limit "10" offset "0"
+    Then si ottiene status code 200
+    When viene invocata l'API di recupero utenze per l'istituzione: "PA1"
+    Then si verifica che la chiamata a selfcare abbia ritornato uno status code: 200
+    And si verifica che le utenze recuperate siano presenti nella lista di utenti appartenenti al tenant del chiamante
