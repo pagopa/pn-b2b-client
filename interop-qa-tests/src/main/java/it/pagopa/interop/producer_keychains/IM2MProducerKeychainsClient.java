919a215e94c96bd3e242dc28510e08fa01007f29
gveldorale <giuseppe.veldorale@alten.it>
2026-02-18 19:16:44 +0100

    QA-12314 feat(IM2MProducerKeychainsClient): extends Authenticable

--- interop-qa-tests/src/main/java/it/pagopa/interop/producer_keychains/IM2MProducerKeychainsClient.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/producer_keychains/IM2MProducerKeychainsClient.java
@@ -1,11 +1,12 @@
 package it.pagopa.interop.producer_keychains;
 
+import it.pagopa.interop.authorization.service.utils.Authenticable;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.KeySeed;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.ProducerKey;
 
 import java.util.UUID;
 
-public interface IM2MProducerKeychainsClient {
+public interface IM2MProducerKeychainsClient extends Authenticable {
 
     ProducerKey createProducerKeychainKey(UUID keychainId, KeySeed keySeed);
 
