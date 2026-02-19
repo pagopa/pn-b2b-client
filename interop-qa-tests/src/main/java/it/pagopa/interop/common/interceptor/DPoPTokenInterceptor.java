9c86e1f056870c3ff20946493e2b2bd0bb7b6540
gveldorale <giuseppe.veldorale@alten.it>
2026-02-19 10:23:25 +0100

    QA-12314 feat(DPoPTokenInterceptor): handle DPoP header

---
+++ interop-qa-tests/src/main/java/it/pagopa/interop/common/interceptor/DPoPTokenInterceptor.java
@@ -0,0 +1,28 @@
+package it.pagopa.interop.common.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class DPoPTokenInterceptor implements ClientHttpRequestInterceptor {
+
+    private final java.util.function.Supplier<String> tokenSupplier;
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        String token = tokenSupplier.get();
+        if (token == null || token.isBlank()) {
+            throw new IllegalStateException("Token vuoto/null");
+        }
+
+        request.getHeaders().set("Authorization", "DPoP " + token);
+
+        return execution.execute(request, body);
+    }
+}
+
