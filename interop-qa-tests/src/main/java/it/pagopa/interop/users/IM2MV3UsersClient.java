0d0d907bed189a37438edce0d3cd259645dc8b46
99niccoloCacace <niccolo.cacace@alten.it>
2026-02-20 11:56:37 +0100

    QA-12314 feat: aggiunta DPoP M2MV3UsersClient

--- interop-qa-tests/src/main/java/it/pagopa/interop/users/IM2MV3UsersClient.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/users/IM2MV3UsersClient.java
@@ -1,11 +1,12 @@
 package it.pagopa.interop.users;
 
+import it.pagopa.interop.authorization.service.utils.Authenticable;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.User;
 import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.Users;
 import java.util.List;
 import java.util.UUID;
 
-public interface IM2MV3UsersClient {
+public interface IM2MV3UsersClient extends Authenticable {
 
 	User getUser(UUID userId);
 
