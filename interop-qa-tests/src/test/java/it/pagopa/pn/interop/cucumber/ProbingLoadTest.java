ce6f81bd1ad8333863a4e8310258a54961a96123
gveldorale <giuseppe.veldorale@alten.it>
2026-02-23 11:56:59 +0100

    QA-12314 fix(ProbingLoadTest): ExcludeTags

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/ProbingLoadTest.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/ProbingLoadTest.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," + "html:target/cucumber-report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.interop.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread")
-@ExcludeTags({"wait_for_fix", "ignore", "loadTest"})
+@ExcludeTags({"wait_for_fix", "ignore"})
 @IncludeTags({"loadTest"})
 public class ProbingLoadTest {
 }
