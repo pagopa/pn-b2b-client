b9d66b776ab33186c4db87b7cf9ab5706c26748a
Emanuele Scozzafava <manu71@gmail.com>
2026-03-16 14:13:31 +0100

    QA-12770 feature: improved createAttributes method to handle Attribute objects and store created attributes

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/attribute/AttributeCommonSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/attribute/AttributeCommonSteps.java
@@ -7,13 +7,15 @@ import io.cucumber.java.en.Then;
 import it.pagopa.interop.authorization.domain.TenantType;
 import it.pagopa.interop.authorization.service.identity.IdentityService;
 import it.pagopa.interop.common.IHttpExecutor;
+import it.pagopa.interop.generated.openapi.clients.bff.model.Attribute;
 import it.pagopa.interop.generated.openapi.clients.bff.model.AttributeKind;
 import it.pagopa.interop.generated.openapi.clients.bff.model.Attributes;
-import it.pagopa.interop.utils.HttpCallExecutor;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.datapreparationservice.BFFDataPreparationService;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import it.pagopa.pn.interop.cucumber.steps.common.AttributeCommonContext;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -42,17 +44,27 @@ public class AttributeCommonSteps {
     public void createAttributes(TenantType tenantType, int count, AttributeKind attributeKind) {
         clientTokenConfigurator.setBearerToken(identityService.getToken(tenantType.name(), null));
         @SuppressWarnings("unchecked")
-        CompletableFuture<UUID>[] futures = new CompletableFuture[count];
+        CompletableFuture<Attribute>[] futures = new CompletableFuture[count];
 
         for(int i = 0; i < count; i++) {
             int finalI = i;
             futures[i] = CompletableFuture.supplyAsync(() -> dataPreparationService.createAttribute(
                 attributeKind,
-                "attribute-%d-%d-%s".formatted(finalI, sharedStepsContext.getTestSeed(), attributeKind)).getId());
+                "attribute-%d-%d-%s".formatted(finalI, sharedStepsContext.getTestSeed(), attributeKind)));
         }
 
         CompletableFuture.allOf(futures).join();
-        attributeCommonContext.setAttributeId(futures[0].join());
+        attributeCommonContext.setAttributeId(futures[0].join().getId());
+        List<Attribute> createdAttributes = Arrays.stream(futures).map(CompletableFuture::join).toList();
+        attributeCommonContext.setCreatedAttributes(createdAttributes);
+
+        List<UUID> attributeIds = createdAttributes.stream().map(Attribute::getId).toList();
+
+        switch (attributeKind) {
+            case CERTIFIED -> attributeCommonContext.setRequiredCertifiedAttributes(List.of(attributeIds));
+            case DECLARED -> attributeCommonContext.setRequiredDeclaredAttributes(List.of(attributeIds));
+            case VERIFIED -> attributeCommonContext.setRequiredVerifiedAttributes(List.of(attributeIds));
+        }
     }
 
     @Given("{tenantType} ha già creato un attributo {attributeKind} con nome che contiene {string}")
