babe1a45024cd663c16a584da00875aa817eca5c
Emanuele Scozzafava <manu71@gmail.com>
2026-03-23 14:43:01 +0100

    QA-12770 refactor: enhance purpose ID resolution in PurposesSteps

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/apiv3/purposes/PurposesSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/apiv3/purposes/PurposesSteps.java
@@ -7,7 +7,8 @@ import io.cucumber.java.en.Given;
 import it.pagopa.interop.purpose.service.IM2MV3PurposeClient;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
-import org.junit.jupiter.api.Assertions;
+import it.pagopa.pn.interop.cucumber.steps.m2m.common.utils.BaseResolver;
+import it.pagopa.pn.interop.cucumber.utility.enums.ResolvableToken;
 
 import java.util.Map;
 import java.util.UUID;
@@ -15,10 +16,12 @@ import java.util.UUID;
 public class PurposesSteps {
     private final IM2MV3PurposeClient purposeClient;
     private final SharedStepsContext sharedStepsContext;
+    private final BaseResolver purposesResolver;
 
     public PurposesSteps(ClientTokenConfigurator clientTokenConfigurator, SharedStepsContext sharedStepsContext) {
         this.purposeClient = clientTokenConfigurator.getM2mV3PurposeClient();
         this.sharedStepsContext = sharedStepsContext;
+        this.purposesResolver = new BaseResolver(sharedStepsContext);
     }
 
     @Given("i residui relativi alle dailyCalls associati alla finalità sono pari a per m2m:")
@@ -28,7 +31,16 @@ public class PurposesSteps {
         int expectedRemainingDailyCallsPerConsumer = Integer.parseInt(expectedData.get("remainingDailyCallsPerConsumer"));
         int expectedRemainingDailyCallsTotals = Integer.parseInt(expectedData.get("remainingDailyCallsTotals"));
 
-        UUID purposeId = sharedStepsContext.getPurposeCommonContext().getPurposeIdAsUUID();
+        // UUID purposeId = this.sharedStepsContext.getPurposeCommonContext().getPurposeIdAsUUID();
+
+        UUID purposeId = this.purposesResolver.resolveOrParse(
+                ResolvableToken.ACTUAL.value(),
+                null,
+                () -> this.sharedStepsContext.getPurposeCommonContext().getPurposeIdAsUUID(),
+                null,
+                null,
+                null
+        );
 
         // TODO Threshold
         /*sharedStepsContext.getHttpCallExecutor().performCall(
@@ -43,11 +55,14 @@ public class PurposesSteps {
     @Given("l'utente cerca di recuperare le soglie rimanenti per la finalità con ID {string} per m2m")
     public void getRemainingDailyCallsM2M(String purposeId) {
 
-        UUID purposeIdAsUUID = switch (purposeId) {
-            case "%null" -> null;
-            case "%random" -> UUID.randomUUID();
-            default -> throw new IllegalStateException("Unexpected value: " + purposeId);
-        };
+        UUID purposeIdAsUUID = this.purposesResolver.resolveOrParse(
+                purposeId,
+                null,
+                () -> this.sharedStepsContext.getPurposeCommonContext().getPurposeIdAsUUID(),
+                null,
+                UUID::randomUUID,
+                null
+        );
 
         // TODO Threshold
         /*sharedStepsContext.getHttpCallExecutor().performCall(
