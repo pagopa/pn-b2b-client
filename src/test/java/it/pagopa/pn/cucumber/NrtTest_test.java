9b86fc2e874dbe4989f557a92609a36e8d32b1cc
mmancini95 <matteo.mancini@pagopa.it>
2026-03-25 11:14:14 +0100

    feat: ripristinate modifiche in NrtTest_test

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
@@ -15,8 +15,9 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore","uat", "integration","realNR","mockNormalizzatore","giacenza890Complex","raddAlternativeCsv", "rasterScartoCON996"})
 @IncludeTags({"workflowDigitale", "workflowAnalogico", "pagamentiMultipli","giacenza890Simplified",
-            "Async", "f24", "version","AOO_UO", "Annullamento",
+            "Async", "f24", "version","AOO_UO", "Annullamento", "raddTechnicalAnnex", "raddAlt",
             "validation", "RetentionAllegati", "apiKeyManager", "downtimeLogs", "recuperoDisservizi",
-            "legalFact", "letturaDestinatario", "restApiValidation", "appIo", "delegheTemporanee"}) //TMP EXCLUDED: ,"partitaIva","raddAlt"
+            "legalFact", "letturaDestinatario", "raddAnagrafica", "raddAttoIntero", "restApiValidation",
+            "appIo", "delegheTemporanee"}) //TMP EXCLUDED: ,"partitaIva","raddAlt"
 public class NrtTest_test {
 }
