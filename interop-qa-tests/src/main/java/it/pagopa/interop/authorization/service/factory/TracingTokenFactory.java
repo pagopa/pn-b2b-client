2ecbe5f3257fdf1b6fbf89f48dbd3bc697841f5c
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-04-07 16:34:11 +0200

    feat(tracing-refactoring)[QA-13610] set current working well-know link

--- interop-qa-tests/src/main/java/it/pagopa/interop/authorization/service/factory/TracingTokenFactory.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/authorization/service/factory/TracingTokenFactory.java
@@ -13,7 +13,12 @@ import software.amazon.awssdk.services.kms.KmsClient;
 @Getter
 @Setter
 public class TracingTokenFactory extends SessionTokenFactory {
-    private static final String WELLKNOWN_URL = "https://tracing-qa-only-well-known-qa.s3.eu-south-1.amazonaws.com/.well-known/jwks.json";
+
+    // Current working well-known DEV link:
+    private static final String WELLKNOWN_URL = "https://dev.interop.pagopa.it/.well-known/jwks.json";
+    // Old  not working well-known QA link:
+    //"https://tracing-qa-only-well-known-qa.s3.eu-south-1.amazonaws.com/.well-known/jwks.json";
+
     private Map<String, Map<String, List<String>>> cachedTokens = null;
 
     public TracingTokenFactory(
