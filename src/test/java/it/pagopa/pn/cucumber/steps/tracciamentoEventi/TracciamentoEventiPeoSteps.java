eb3e6603f1c622424695e1995703e36bee906594
Matteo Sperati <matteo.sperati@dgsspa.com>
2026-04-08 15:01:46 +0200

    feature:qa12928 implementazione aggiunti controlli su evento M003 (sempre presente) e rivisitato metodi in modo da poter usare client diversi da pn-test

--- src/test/java/it/pagopa/pn/cucumber/steps/tracciamentoEventi/TracciamentoEventiPeoSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/tracciamentoEventi/TracciamentoEventiPeoSteps.java
@@ -3,10 +3,11 @@ package it.pagopa.pn.cucumber.steps.tracciamentoEventi;
 import com.jayway.jsonpath.JsonPath;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import it.pagopa.pn.client.b2b.pa.service.IPnSafeStoragePrivateClient;
+import it.pagopa.pn.client.b2b.pa.service.impl.PnSafeStoragePrivateClientImpl;
 import it.pagopa.pn.client.web.generated.openapi.clients.safeStorage.model.FileCreationRequest;
 import it.pagopa.pn.client.web.generated.openapi.clients.safeStorage.model.FileCreationResponse;
-import it.pagopa.pn.client.web.generated.openapi.clients.safeStorage.model.FileDownloadResponse;
 import it.pagopa.pn.cucumber.steps.SharedSteps;
 import it.pagopa.pn.cucumber.steps.pa.utilityVersions.B2bUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +26,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -33,21 +36,28 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class TracciamentoEventiPeoSteps {
 
     private final SharedSteps sharedSteps;
-    private final IPnSafeStoragePrivateClient safeStorageClient;
+    private final PnSafeStoragePrivateClientImpl safeStorageClient;
     private final String safeStorageBaseUrl;
+    private String clientInUse;
     private String requestId;
     private static final String EICAR = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
 
     @Autowired
     public TracciamentoEventiPeoSteps(SharedSteps sharedSteps,
-                                      IPnSafeStoragePrivateClient safeStorageClient,
+                                      PnSafeStoragePrivateClientImpl safeStorageClient,
                                       @Value("${pn.safeStorage.base-url}") String safeStorageBaseUrl) {
         this.sharedSteps = sharedSteps;
         this.safeStorageClient = safeStorageClient;
         this.safeStorageBaseUrl = safeStorageBaseUrl;
     }
 
-    @Given("viene inviata una mail tramite PEO all'indirizzo {string} con allegato {string}")
+    @Given("il client in uso è {string}")
+    public void ilClientInUsoÈ(String clientId) {
+        this.clientInUse = clientId;
+        safeStorageClient.customApiClient(clientId);
+    }
+
+    @When("viene inviata una mail tramite PEO all'indirizzo {string} con allegato {string}")
     public void sendEmailWithAttachment(String emailAddress, String attachmentType) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         String timestamp = Instant.now().toString();
@@ -69,7 +79,7 @@ public class TracciamentoEventiPeoSteps {
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(safeStorageBaseUrl + "/external-channels/v1/digital-deliveries/courtesy-full-message-requests/" + requestId))
-                .header("x-pagopa-extch-cx-id", "pn-test")
+                .header("x-pagopa-extch-cx-id", clientInUse)
                 .header("Content-Type", "application/json")
                 .header("Accept", "application/json")
                 .PUT(HttpRequest.BodyPublishers.ofString(jsonBody))
@@ -99,37 +109,27 @@ public class TracciamentoEventiPeoSteps {
         }
     }
 
-    @Then("recuperando la request da gestore-repository, verifico che il record contenga un evento con statusCode {string} e status {string}")
-    public void checkPnEcRichiesteMetadatiRecord(String statusCode, String status) throws IOException, InterruptedException {
-        sleep();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(safeStorageBaseUrl + "/external-channel/gestoreRepository/requests/" + requestId))
-                .header("x-pagopa-extch-cx-id", "pn-test")
-                .header("Accept", "application/json")
-                .GET()
-                .build();
+    private String uploadEicarVirusFile() {
+        byte[] byteArray = EICAR.getBytes(StandardCharsets.US_ASCII);
+        String sha256 = B2bUtils.computeSha256(new ByteArrayInputStream(byteArray));
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Errore nella chiamata GET: " + response.statusCode() + " - " + response.body());
-        }
-        String prettyJson = B2bUtils.logPrettyResponse(response.body());
-        checkResponseFields(prettyJson, statusCode, status);
-    }
+        FileCreationRequest request = new FileCreationRequest();
+        request.setStatus("SAVED");
+        request.setContentType("text/plain");
+        request.setDocumentType("PN_NOTIFICATION_ATTACHMENTS");
 
-    private void checkResponseFields(String json, String statusCode, String status) {
-        Object eventsList = JsonPath.read(json, "$.requestMetadata.eventsList");
-        assertThat(eventsList).as("Il campo eventsList deve esistere").isNotNull();
-        assertThat(eventsList).isInstanceOf(JSONArray.class);
-        String filter = String.format(
-                "$.requestMetadata.eventsList[?(@.digProgrStatus.statusCode == '%s' && @.digProgrStatus.status == '%s')]",
-                statusCode, status
-        );
-        JSONArray filteredEvents = JsonPath.read(json, filter);
-        assertThat(filteredEvents)
-                .as("L'eventsList dovrebbe contenere un evento con statusCode '%s' e status '%s'", statusCode, status)
-                .asList().isNotEmpty();
+        // Chiamata a Safe Storage per registrare il file e ottenere la presigned url di upload
+        ResponseEntity<FileCreationResponse> responseEntity = safeStorageClient.createFileWithHttpInfo(clientInUse, sha256, "SHA256", request);
+        assertThat(responseEntity).as("La responseEntity non dev'essere null").isNotNull();
+        FileCreationResponse fileCreationResponse = responseEntity.getBody();
+        assertThat(fileCreationResponse).as("La FileCreationResponse non dev'essere null").isNotNull();
+
+        String fileKey = fileCreationResponse.getKey();
+        String secret = fileCreationResponse.getSecret();
+        String url = fileCreationResponse.getUploadUrl();
+
+        B2bUtils.loadToPresignedFromByteArray(sharedSteps.getContext(), url, secret, sha256, byteArray, "text/plain");
+        return "safestorage://" + fileKey;
     }
 
     private void sleep() {
@@ -141,27 +141,48 @@ public class TracciamentoEventiPeoSteps {
         }
     }
 
-    private String uploadEicarVirusFile() {
-        byte[] byteArray = EICAR.getBytes(StandardCharsets.US_ASCII);
-        String sha256 = B2bUtils.computeSha256(new ByteArrayInputStream(byteArray));
+    @Then("recuperando la request da gestore-repository, verifico che il record abbia un'eventsList coi seguenti eventi {string}")
+    public void recuperandoLaRequestDaGestoreRepositoryVerificoCheIlRecordContengaUnEventoConEventsList(String events) throws IOException, InterruptedException {
+        sleep();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(safeStorageBaseUrl + "/external-channel/gestoreRepository/requests/" + requestId))
+                .header("x-pagopa-extch-cx-id", clientInUse)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
 
-        FileCreationRequest request = new FileCreationRequest();
-        request.setStatus("SAVED");
-        request.setContentType("text/plain");
-        request.setDocumentType("PN_NOTIFICATION_ATTACHMENTS");
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Errore nella chiamata GET: " + response.statusCode() + " - " + response.body());
+        }
+        String prettyJson = B2bUtils.logPrettyResponse(response.body());
+        checkEventsList(prettyJson, events);
+    }
 
-        // Chiamata a Safe Storage per registrare il file e ottenere la presigned url di upload
-        ResponseEntity<FileCreationResponse> responseEntity = safeStorageClient.createFileWithHttpInfo("pn-test", sha256, "SHA256", request);
-        assertThat(responseEntity).as("La responseEntity non dev'essere null").isNotNull();
-        FileCreationResponse fileCreationResponse = responseEntity.getBody();
-        assertThat(fileCreationResponse).as("La FileCreationResponse non dev'essere null").isNotNull();
+    private void checkEventsList(String json, String events) {
+        Object eventsList = JsonPath.read(json, "$.requestMetadata.eventsList");
+        assertThat(eventsList).as("Il campo eventsList deve esistere").isNotNull();
+        assertThat(eventsList).isInstanceOf(JSONArray.class);
 
-        String fileKey = fileCreationResponse.getKey();
-        String secret = fileCreationResponse.getSecret();
-        String url = fileCreationResponse.getUploadUrl();
-
-        B2bUtils.loadToPresignedFromByteArray(sharedSteps.getContext(), url, secret, sha256, byteArray, "text/plain");
-        return "safestorage://" + fileKey;
-
+        String[] eventsExpected = events.split(";");
+        Map<String, String> mapCodeStatus = new HashMap<>();
+        for (int i = 0; i < eventsExpected.length; i++) {
+            String event = eventsExpected[i];
+            String[] codeStatus = event.split("-");
+            mapCodeStatus.put(codeStatus[0], codeStatus[1]);
+        }
+        mapCodeStatus.entrySet().forEach(entry -> {
+            String statusCode = entry.getKey();
+            String status = entry.getValue();
+            String filter = String.format(
+                    "$.requestMetadata.eventsList[?(@.digProgrStatus.statusCode == '%s' && @.digProgrStatus.status == '%s')]",
+                    statusCode, status
+            );
+            JSONArray filteredEvents = JsonPath.read(json, filter);
+            assertThat(filteredEvents)
+                    .as("L'eventsList dovrebbe contenere un evento con statusCode '%s' e status '%s'", statusCode, status)
+                    .asList().isNotEmpty();
+        });
     }
 }
--- src/test/resources/it/pagopa/pn/cucumber/visualizzazioneNotifica/TracciamentoEventiPEO.feature
+++ src/test/resources/it/pagopa/pn/cucumber/visualizzazioneNotifica/TracciamentoEventiPEO.feature
@@ -2,12 +2,20 @@ Feature: irrobustimento tracciamento eventi PEO PostaElettronicaOrdinaria (SRS: 
 
   @TracciamentoEventiPEO
   Scenario Outline: [CHECK_EVENT_STATUS]
+    Given il client in uso è "<client>"
     When viene inviata una mail tramite PEO all'indirizzo "<emailAddress>" con allegato "<attachment>"
-    Then recuperando la request da gestore-repository, verifico che il record contenga un evento con statusCode "<statusCode>" e status "<status>"
+    Then recuperando la request da gestore-repository, verifico che il record abbia un'eventsList coi seguenti eventi "<events>"
     Examples:
-      | emailAddress                            | attachment | statusCode | status    |
-      | matteo.sperati@dgsspa.com               | null       | M004       | delivered |
-      | bounce@simulator.amazonses.com          | null       | M005       | bounced   |
-      | suppressionlist@simulator.amazonses.com | null       | M005       | bounced   |
-      | complaint@simulator.amazonses.com       | null       | M006       | spam      |
-      | matteo.sperati@dgsspa.com               | virus      | M009       | rejected  |
+      | client      | emailAddress                            | attachment | events                   |
+      #Client con eventi PEO censiti su pn-EcAnagrafica (confinfo)
+      | pn-test     | matteo.sperati@dgsspa.com               | null       | M003-sent;M004-delivered |
+      | pn-test     | bounce@simulator.amazonses.com          | null       | M003-sent;M005-bounced   |
+      | pn-test     | suppressionlist@simulator.amazonses.com | null       | M003-sent;M005-bounced   |
+      | pn-test     | complaint@simulator.amazonses.com       | null       | M003-sent;M006-spam      |
+      | pn-test     | matteo.sperati@dgsspa.com               | virus      | M003-sent;M009-rejected  |
+      #Client con eventi PEO non censiti su pn-EcAnagrafica (tranne M003, che è di default)
+      | pn-delivery | matteo.sperati@dgsspa.com               | null       | M003-sent                |
+      | pn-delivery | bounce@simulator.amazonses.com          | null       | M003-sent                |
+      | pn-delivery | suppressionlist@simulator.amazonses.com | null       | M003-sent                |
+      | pn-delivery | complaint@simulator.amazonses.com       | null       | M003-sent                |
+      | pn-delivery | matteo.sperati@dgsspa.com               | virus      | M003-sent                |
