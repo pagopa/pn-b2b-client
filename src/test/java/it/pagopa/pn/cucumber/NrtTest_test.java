fcfecd9046db72a47f8808481b8bee193d9e2ec1
mmancini95 <matteo.mancini@pagopa.it>
2026-04-10 14:18:27 +0200

    feat(retry-nrt): aggiunto il salvataggio dei test falliti nel failed.txt nel caso di Nrt

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
@@ -11,6 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
         "html:target/cucumber-report.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "rerun:target/failed.txt")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore","uat", "integration","realNR","mockNormalizzatore","giacenza890Complex","raddAlternativeCsv", "rasterScartoCON996"})
