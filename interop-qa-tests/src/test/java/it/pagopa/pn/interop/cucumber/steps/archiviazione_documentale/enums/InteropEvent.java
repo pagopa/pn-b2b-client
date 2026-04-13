76a86df6d9d267493a5526ba1b1cb1cbf2e1002c
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-13 21:44:00 +0200

    feat(InteropEvent): add new event types for EService and Purpose templates

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/enums/InteropEvent.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/archiviazione_documentale/enums/InteropEvent.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum InteropEvent {
+    ESERVICE_ADDED("EServiceAdded"),
+    ESERVICE_DESCRIPTOR_PUBLISHED("EServiceDescriptorPublished"),
+    DRAFT_ESERVICE_UPDATED("DraftEServiceUpdated"),
     AGREEMENT_ACTIVATED("AgreementActivated"),
     AGREEMENT_UPGRADED("AgreementUpgraded"),
     AGREEMENT_SUSPENDED_BY_CONSUMER("AgreementSuspendedByConsumer"),
@@ -20,7 +23,12 @@ public enum InteropEvent {
     CLIENT_KEY_DELETED("ClientKeyDeleted"),
     DESCRIPTOR_ESERVICE_UPGRADED("DescriptorEServiceUpgraded"),
     CLIENT_DELETE("ClientDelete"),
-    CLIENT_KEY_ADDED("ClientKeyAdded");
+    CLIENT_KEY_ADDED("ClientKeyAdded"),
+    RISK_ANALYSIS_TEMPLATE_DOCUMENT_GENERATED("RiskAnalysisTemplateDocumentGenerated"),
+    PURPOSE_TEMPLATE_SUSPENDED("PurposeTemplateSuspended"),
+    PURPOSE_TEMPLATE_UNSUSPENDED("PurposeTemplateUnsuspended"),
+    PURPOSE_TEMPLATE_ARCHIVED("PurposeTemplateArchived"),
+    PURPOSE_TEMPLATE_PUBLISHED("PurposeTemplatePublished");
 
     private final String value;
 
