7e24554db399843551270236da511a42fa159d0e
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-13 21:45:29 +0200

    feat(IM2MEventClient): add methods for retrieving events and events after a specific ID

--- interop-qa-tests/src/main/java/it/pagopa/interop/event/service/IM2MEventClient.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/event/service/IM2MEventClient.java
@@ -3,6 +3,10 @@ package it.pagopa.interop.event.service;
 import it.pagopa.interop.authorization.service.utils.SettableBearerToken;
 import it.pagopa.interop.event.domain.M2MEventRequest;
 import it.pagopa.interop.event.domain.M2MEvents;
+import it.pagopa.interop.event.queue.purpose_template.PurposeTemplateM2MEvents;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface IM2MEventClient extends SettableBearerToken {
     M2MEvents getEServicesEvents(M2MEventRequest request);
@@ -28,4 +32,8 @@ public interface IM2MEventClient extends SettableBearerToken {
     M2MEvents getPurposeEvents(M2MEventRequest request);
 
     M2MEvents getTenantEvents(M2MEventRequest request);
+
+    <Event, EventRequest> List<Event> getEvents(EventRequest request);
+
+    <Event, EventRequest> List<Event> getEventsAfter(EventRequest request, UUID lastEventId);
 }
