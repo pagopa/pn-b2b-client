e1d80b5002461bababeab4d2703eb7c3f1668d94
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-14 09:54:40 +0200

    refactor: delete unsed EventsSteps.java, IEventQueueDispatcher.java

--- interop-qa-tests/src/main/java/it/pagopa/interop/event/queue/dispatcher/IEventQueueDispatcher.java
+++
@@ -1,8 +0,0 @@
-package it.pagopa.interop.event.queue.dispatcher;
-
-import it.pagopa.interop.event.queue.IEventQueue;
-
-public interface IEventQueueDispatcher {
-    <EventFilter> boolean canHandle(EventFilter eventFilter);
-     IEventQueue getQueue();
-}
