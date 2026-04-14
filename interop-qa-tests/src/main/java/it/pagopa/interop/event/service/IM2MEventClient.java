4e8d80eb48b186e3f32dac5ba635e8d74005a930
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-14 09:50:21 +0200

    feat(IM2MEventClient): add method for retrieving purpose template events

--- interop-qa-tests/src/main/java/it/pagopa/interop/event/service/IM2MEventClient.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/event/service/IM2MEventClient.java
@@ -3,10 +3,6 @@ package it.pagopa.interop.event.service;
 import it.pagopa.interop.authorization.service.utils.SettableBearerToken;
 import it.pagopa.interop.event.domain.M2MEventRequest;
 import it.pagopa.interop.event.domain.M2MEvents;
-import it.pagopa.interop.event.queue.purpose_template.PurposeTemplateM2MEvents;
-
-import java.util.List;
-import java.util.UUID;
 
 public interface IM2MEventClient extends SettableBearerToken {
     M2MEvents getEServicesEvents(M2MEventRequest request);
@@ -33,7 +29,5 @@ public interface IM2MEventClient extends SettableBearerToken {
 
     M2MEvents getTenantEvents(M2MEventRequest request);
 
-    <Event, EventRequest> List<Event> getEvents(EventRequest request);
-
-    <Event, EventRequest> List<Event> getEventsAfter(EventRequest request, UUID lastEventId);
+    M2MEvents getPurposeTemplateEvents(M2MEventRequest request);
 }
