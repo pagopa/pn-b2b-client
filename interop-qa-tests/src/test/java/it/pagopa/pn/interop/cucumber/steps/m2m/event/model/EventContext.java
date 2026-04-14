8a2aa23fdb47fd8e87ecb6286aab8c9e57d6fa21
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-14 17:54:21 +0200

    refactor(EventContext): update event storage to use a map for last matched events

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/event/model/EventContext.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/m2m/event/model/EventContext.java
@@ -1,11 +1,22 @@
 package it.pagopa.pn.interop.cucumber.steps.m2m.event.model;
 
-import it.pagopa.interop.event.queue.purpose_template.PurposeTemplateM2MEvent;
+import it.pagopa.interop.event.domain.dto.M2MEvent;
+import it.pagopa.interop.event.enums.InteropEvent;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Map;
 
 @Getter
 @Setter
 public class EventContext {
-    private PurposeTemplateM2MEvent lastPurposeTemplateEventMatched;
+    private Map<InteropEvent, M2MEvent> lastEvents;
+
+    public M2MEvent getLastEventMatched(InteropEvent interopEvent) {
+        return lastEvents.get(interopEvent);
+    }
+
+    public void setLastEventMatched(InteropEvent interopEvent, M2MEvent lastEvent) {
+        lastEvents.put(interopEvent, lastEvent);
+    }
 }
