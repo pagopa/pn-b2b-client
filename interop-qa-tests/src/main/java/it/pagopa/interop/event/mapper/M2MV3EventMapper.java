469db9978186b9e705b727c965e8cf559c2b181b
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-04-15 14:33:45 +0200

    feat(event-mappers): add mapping for purposeTemplateId in M2MV3EventMapper and M2MEventMapper

--- interop-qa-tests/src/main/java/it/pagopa/interop/event/mapper/M2MV3EventMapper.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/event/mapper/M2MV3EventMapper.java
@@ -11,6 +11,7 @@ import java.time.OffsetDateTime;
 @Mapper(componentModel = "spring")
 public interface M2MV3EventMapper {
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "eserviceId", source = "eserviceId")
     @Mapping(target = "producerDelegationId", source = "producerDelegationId")
     @Mapping(target = "descriptorId", source = "descriptorId")
@@ -18,6 +19,7 @@ public interface M2MV3EventMapper {
     @Mapping(target = "eventType", source = "eventType")
     M2MEserviceEvent map(EServiceEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "agreementId", source = "agreementId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
@@ -25,11 +27,13 @@ public interface M2MV3EventMapper {
     @Mapping(target = "consumerDelegationId", source = "consumerDelegationId")
     M2MAgreementEvent map(AgreementEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "attributeId", source = "attributeId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MAttributeEvent map(AttributeEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "purposeId", source = "purposeId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
@@ -37,48 +41,62 @@ public interface M2MV3EventMapper {
     @Mapping(target = "consumerDelegationId", source = "consumerDelegationId")
     M2MPurposeEvent map(PurposeEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "tenantId", source = "tenantId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MTenantEvent map(TenantEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "eserviceTemplateId", source = "eserviceTemplateId")
     @Mapping(target = "eserviceTemplateVersionId", source = "eserviceTemplateVersionId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MEServiceTemplateEvent map(EServiceTemplateEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "kid", source = "kid")
     @Mapping(target = "clientId", source = "clientId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MKeyEvent map(KeyEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "clientId", source = "clientId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MClientEvent map(ClientEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "kid", source = "kid")
     @Mapping(target = "producerKeychainId", source = "producerKeychainId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MProducerKeyEvent map(ProducerKeyEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "producerKeychainId", source = "producerKeychainId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MProducerKeychainEvent map(ProducerKeychainEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "delegationId", source = "delegationId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MProducerDelegationEvent map(ProducerDelegationEvent event);
 
+    @Mapping(target = "id", source = "id")
     @Mapping(target = "delegationId", source = "delegationId")
     @Mapping(target = "eventTimestamp", source = "eventTimestamp")
     @Mapping(target = "eventType", source = "eventType")
     M2MConsumerDelegationEvent map(ConsumerDelegationEvent event);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "purposeTemplateId", source = "purposeTemplateId")
+    @Mapping(target = "eventTimestamp", source = "eventTimestamp")
+    @Mapping(target = "eventType", source = "eventType")
+    M2MPurposeTemplateEvent map(PurposeTemplateEvent event);
 
     M2MEServiceEvents map(EServiceEvents event);
     M2MAgreementEvents map(AgreementEvents event);
@@ -92,8 +110,7 @@ public interface M2MV3EventMapper {
     M2MProducerKeychainEvents map(ProducerKeychainEvents event);
     M2MProducerDelegationEvents map(ProducerDelegationEvents event);
     M2MConsumerDelegationEvents map(ConsumerDelegationEvents event);
-    //TODO: decommentare per la feature di eventi finalità agevoltata
-    //M2MPurposeTemplateEvents map(PurposeTemplateEvents event);
+    M2MPurposeTemplateEvents map(PurposeTemplateEvents event);
 
     /* TODO 28/11/2025: questa logica di conversione compare identica altrove in più occasioni.
         Andrebbe incapsulata in una utility condivisa. */
