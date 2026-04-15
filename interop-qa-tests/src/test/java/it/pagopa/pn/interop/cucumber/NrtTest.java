1814783a7c559c7a6db0a001602d1af16cb01102
99niccoloCacace <niccolo.cacace@alten.it>
2026-04-15 11:28:37 +0200

    QA-13477 chore: add eservice_published_delegation tests to nrt

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/NrtTest.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/NrtTest.java
@@ -30,15 +30,15 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 })
 @ExcludeTags({"wait_for_fix", "ignore"})
 @IncludeTags({
-    // BFF
-    "agreement", "attribute", "descriptor", "document", "eservice", "purpose", "daily_calls_update_request",
-    "purpose_latest_risk_analysis", "purpose_risk_analysis", "incaricato", "capofila", "selfcare",
-    "app-edit-ff-on", "llgg", "e-service-template", "e-service-template-receive-bff", "purposeTemplate",
+        // BFF
+        "agreement", "attribute", "descriptor", "document", "eservice", "purpose", "daily_calls_update_request",
+        "purpose_latest_risk_analysis", "purpose_risk_analysis", "incaricato", "capofila", "selfcare",
+        "app-edit-ff-on", "llgg", "e-service-template", "e-service-template-receive-bff", "purposeTemplate",
 
-    // M2M
-    "m2m-agreements", "m2m-purposes", "m2m-attributes", "m2m-eservices", "m2m-agreements-parte2-luglio",
-    "m2m-parte2-agosto-rilascio1", "m2m-parte2-agosto-rilascio2", "m2m-parte2-settembre",
-    "m2m-parte2-ottobre", "m2mEservices", "e-service-template-receive-m2m"
+        // M2M
+        "m2m-agreements", "m2m-purposes", "m2m-attributes", "m2m-eservices", "m2m-agreements-parte2-luglio",
+        "m2m-parte2-agosto-rilascio1", "m2m-parte2-agosto-rilascio2", "m2m-parte2-settembre",
+        "m2m-parte2-ottobre", "m2mEservices", "e-service-template-receive-m2m", "eservice_published_delegation"
 
 })
 public class NrtTest {
