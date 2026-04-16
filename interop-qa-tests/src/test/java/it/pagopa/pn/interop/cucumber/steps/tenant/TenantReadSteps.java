b4e75092fa461b211c01bc3e70d9b6c6e1db74f8
Vincenzo Massaro <vincenzo.massaro@alten.it>
2026-04-16 17:02:07 +0200

    QA-13875 feat: [TENANT_DELEGATIONS_ALLOWED]

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/tenant/TenantReadSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/tenant/TenantReadSteps.java
@@ -1,11 +1,14 @@
 package it.pagopa.pn.interop.cucumber.steps.tenant;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import it.pagopa.interop.authorization.service.identity.IdentityService;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TenantReadSteps {
     private final ClientTokenConfigurator clientTokenConfigurator;
@@ -26,4 +29,24 @@ public class TenantReadSteps {
                 () -> clientTokenConfigurator.getTenantsApi().getTenant(tenantId)
         );
     }
+
+    @When("l'utente visualizza se all'utente d'appartenenza è permesso partecipare a processi di delega")
+    public void isTenantDelegationsAllowed() {
+        String tenantType = sharedStepsContext.getTenantType();
+        UUID tenantId = identityService.getOrganizationId(tenantType);
+        Boolean tenantAllowedToDelegation = clientTokenConfigurator.getTenantsApi().isTenantAllowedToDelegation(tenantId);
+        sharedStepsContext.getTenantCommonContext().setIsTenantDelegationsAllowed(tenantAllowedToDelegation);
+    }
+
+    @Then("l'utente ottiene responso {booleanResponse} dal sistema sul poter partecipare a processi di delega")
+    public void checkDelegationsAllowed(Boolean response) {
+        Boolean isTenantDelegationsAllowed = sharedStepsContext.getTenantCommonContext().getIsTenantDelegationsAllowed();
+        assertThat(isTenantDelegationsAllowed)
+                .as("Verifica che, indipendentemente dall'esito, la risposta sia comunque valorizzata")
+                .isNotNull();
+        assertThat(isTenantDelegationsAllowed)
+                .as("Verifica che la possibilità di prendere parte a una delega sia coerente con quanto atteso")
+                .isEqualTo(response);
+    }
+
 }
