8fc6e19f425c0fa91007be71eda857763208ba55
Emanuele Scozzafava <manu71@gmail.com>
2026-04-17 12:05:45 +0200

    feat(desc-maxlength): add scenarios for e-service template creation with maximum description length validation

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
@@ -29,6 +29,8 @@ import java.util.List;
 import java.util.UUID;
 
 import lombok.Data;
+import org.jeasy.random.randomizers.text.StringRandomizer;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.http.HttpStatus;
 
 // TODO perché @Data? Considerarne rimozione da questa e dalle altre classi
@@ -100,8 +102,19 @@ public class EServiceTemplateCreateSteps {
 
     @Given("l'utente effettua la creazione di un e-service template in modalità {eServiceMode} in stato di {eServiceTemplateVersionState} con nome {string}")
     public void createEServiceTemplateWithName(EServiceMode eServiceMode, EServiceTemplateVersionState desiredState, String name) {
+        createEServiceTemplateWithName(eServiceMode, desiredState, name, null);
+    }
 
-        EServiceTemplateSeed templateSeed = getEServiceTemplateSeed(eServiceMode, true);
+    @Given("l'utente effettua la creazione di un e-service template in modalità {eServiceMode} in stato di {eServiceTemplateVersionState} con nome {string} e descrizione di {int} caratteri")
+    public void createEServiceTemplateWithName(EServiceMode eServiceMode, EServiceTemplateVersionState desiredState, String name, Integer descriptionLength) {
+
+        EServiceTemplateSeed templateSeed;
+        if (descriptionLength != null) {
+            String description = (new StringRandomizer(descriptionLength, descriptionLength, System.currentTimeMillis())).getRandomValue();
+            templateSeed = getEServiceTemplateSeed(eServiceMode, true, description);
+        } else {
+            templateSeed = getEServiceTemplateSeed(eServiceMode, true);
+        }
 
         EServiceTemplateStepContext ctx = sharedStepsContext.getEServiceTemplateStepContext();
         String seed = ctx.getLastUsedEServiceTemplateNameSeed();
@@ -117,7 +130,9 @@ public class EServiceTemplateCreateSteps {
         if (eServiceMode == EServiceMode.RECEIVE && nonNull(lastTemplateManaged)) {
             testAssistant.addRiskAnalysisToEServiceTemplateSuccessfully(); // perché ogni template in RECEIVE deve avere una risk analysis
         }
-        testAssistant.mutateLastVersionState(desiredState);
+        if (nonNull(lastTemplateManaged)) {
+            testAssistant.mutateLastVersionState(desiredState);
+        }
     }
 
     @When("l'utente effettua la creazione di un e-service template in modalità {eServiceMode} in stato di {eServiceTemplateVersionState} con flagPersonalData impostato a {string}")
@@ -129,6 +144,16 @@ public class EServiceTemplateCreateSteps {
             testAssistant.addRiskAnalysisToEServiceTemplateSuccessfully(); // perché ogni template in RECEIVE deve avere una risk analysis
         }
         testAssistant.mutateLastVersionState(desiredState);
+    }
+
+    @When("l'e-service template creato ha una descrizione di {int} caratteri")
+    public void eServiceTemplateCreatedHasLongDescription(Integer descriptionLength) {
+
+        EServiceTemplateInfo lastTemplateManaged = sharedStepsContext.getEServiceTemplateStepContext()
+                .getLastTemplateManaged();
+
+        Assertions.assertNotNull(lastTemplateManaged.getEServiceDescription());
+        Assertions.assertEquals(lastTemplateManaged.getEServiceDescription().length(), descriptionLength);
     }
 
     @When("{string} porta la versione dell'e-service template in stato {eServiceTemplateVersionState}")
@@ -217,17 +242,7 @@ public class EServiceTemplateCreateSteps {
      * @return a new {@link EServiceTemplateSeed} instance
      */
     private EServiceTemplateSeed getEServiceTemplateSeed(EServiceMode eServiceMode) {
-        String templateName = testAssistant.buildEServiceTemplateName();
-        VersionSeedForEServiceTemplateCreation version = new VersionSeedForEServiceTemplateCreation()
-                .voucherLifespan(86400);
-        return new EServiceTemplateSeed()
-                .intendedTarget("Audience description per il template " + templateName)
-                .name(templateName)
-                .description("Descrizione del servizio associato al template " + templateName)
-                .mode(eServiceMode)
-                .version(version)
-                .personalData(false)
-                .technology(EServiceTechnology.REST);
+        return getEServiceTemplateSeed(eServiceMode, false);
     }
 
     private EServiceTemplateSeed getEServiceTemplateSeed(EServiceMode eServiceMode, Boolean flagPersonalData) {
@@ -238,6 +253,20 @@ public class EServiceTemplateCreateSteps {
                 .intendedTarget("Audience description per il template " + templateName)
                 .name(templateName)
                 .description("Descrizione del servizio associato al template " + templateName)
+                .mode(eServiceMode)
+                .version(version)
+                .personalData(flagPersonalData)
+                .technology(EServiceTechnology.REST);
+    }
+
+    private EServiceTemplateSeed getEServiceTemplateSeed(EServiceMode eServiceMode, Boolean flagPersonalData, String description) {
+        String templateName = testAssistant.buildEServiceTemplateName();
+        VersionSeedForEServiceTemplateCreation version = new VersionSeedForEServiceTemplateCreation()
+                .voucherLifespan(86400);
+        return new EServiceTemplateSeed()
+                .intendedTarget("Audience description per il template " + templateName)
+                .name(templateName)
+                .description(description)
                 .mode(eServiceMode)
                 .version(version)
                 .personalData(flagPersonalData)
--- interop-qa-tests/src/test/resources/it/pagopa/pn/cucumber/e-service-template/e-service-template.feature
+++ interop-qa-tests/src/test/resources/it/pagopa/pn/cucumber/e-service-template/e-service-template.feature
@@ -3082,3 +3082,29 @@ Feature: Test API of e-service template
     When l'utente è un "admin" di "PA2"
     And l'utente recupera le proprie istanze e-service template create dall'e-service template "%actual"
     Then si ottiene response status code 404
+
+  @eservice_description_max_length
+  Scenario: [ESERVICE_TEMPLATE_CREATE_DESCRIPTION_MAX_LENGTH_1] La creazione di un e-service template in DRAFT può essere fatta utilizzando la dimensione massima della descrizione
+    Given l'utente è un "admin" di "PA1"
+    When l'utente effettua la creazione di un e-service template in modalità erogazione in stato di DRAFT con nome "max-descr" e descrizione di 400 caratteri
+    Then si ottiene response status code 200
+    And l'e-service template creato ha una descrizione di 400 caratteri
+
+  @eservice_description_max_length
+  Scenario: [ESERVICE_TEMPLATE_CREATE_DESCRIPTION_MAX_LENGTH_2] La creazione di un e-service template in DRAFT non va a buon fine se viene superata la dimensione massima consentita per la descrizione
+    Given l'utente è un "admin" di "PA1"
+    When l'utente effettua la creazione di un e-service template in modalità erogazione in stato di DRAFT con nome "max-descr" e descrizione di 401 caratteri
+    Then si ottiene response status code 400
+
+  @eservice_description_max_length
+  Scenario: [ESERVICE_TEMPLATE_CREATE_DESCRIPTION_MAX_LENGTH_3] La creazione di un e-service template in stato PUBLISHED può essere fatta utilizzando la dimensione massima della descrizione
+    Given l'utente è un "admin" di "PA1"
+    When l'utente effettua la creazione di un e-service template in modalità erogazione in stato di PUBLISHED con nome "max-descr" e descrizione di 400 caratteri
+    Then si ottiene response status code 200
+    And l'e-service template creato ha una descrizione di 400 caratteri
+
+  @eservice_description_max_length
+  Scenario: [ESERVICE_TEMPLATE_CREATE_DESCRIPTION_MAX_LENGTH_4] La creazione di un e-service template in stato PUBLISHED non va a buon fine se viene superata la dimensione massima consentita per la descrizione
+    Given l'utente è un "admin" di "PA1"
+    When l'utente effettua la creazione di un e-service template in modalità erogazione in stato di PUBLISHED con nome "max-descr" e descrizione di 401 caratteri
+    Then si ottiene response status code 400
