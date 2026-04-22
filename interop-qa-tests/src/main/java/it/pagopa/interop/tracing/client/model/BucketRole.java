f1834b3e7ad94fb8b2decd722424d2b6583e24fd
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-04-22 20:26:16 +0200

    fix(tracing-refactoring)[QA-13610]: remove not necessary bucket objects

--- interop-qa-tests/src/main/java/it/pagopa/interop/tracing/client/model/BucketRole.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.interop.tracing.client.model;
-
-public enum BucketRole {
-    /** Write Once Read Many – archiviazione definitiva */
-    WORM,
-
-    /** Bucket operativo / temporaneo */
-    STANDARD
-}
