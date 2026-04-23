8be422f0c6810eb062677ce3feb1ae4ef5b99e71
Matteo Sperati <matteo.sperati@dgsspa.com>
2026-04-23 10:20:38 +0200

    feat(tracciamentoEventiPeo)[QA12928] creazione classe test suite TracciamentoEventiPeoTest

--- src/test/java/it/pagopa/pn/cucumber/TracciamentoEventiPeoTest.java
+++ src/test/java/it/pagopa/pn/cucumber/TracciamentoEventiPeoTest.java
@@ -1,17 +1,18 @@
 package it.pagopa.pn.cucumber;
 
+import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.*;
-
-import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("it/pagopa/pn/cucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
-        "html:target/cucumber-report.html")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
-@ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty"),
+        @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
+                "html:target/cucumber-report.html"),
+        @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
+        @ConfigurationParameter(key = Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
+})
 @ExcludeTags({"ignore"})
 @IncludeTags({"TracciamentoEventiPEO"})
 public class TracciamentoEventiPeoTest {
