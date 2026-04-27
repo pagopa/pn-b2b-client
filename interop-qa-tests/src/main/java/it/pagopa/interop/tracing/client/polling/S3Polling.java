8fa98704e4b8ba0ab6728d7659503a203dc7b273
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-04-27 09:52:25 +0200

    feat(tracing-refactoring)[QA-13610]: S3 tracing uses credentials profile extra-qa

--- interop-qa-tests/src/main/java/it/pagopa/interop/tracing/client/polling/S3Polling.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/tracing/client/polling/S3Polling.java
@@ -1,11 +1,9 @@
 package it.pagopa.interop.tracing.client.polling;
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.profiles.ProfileFile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
-import java.nio.file.Paths;
 import java.util.function.Predicate;
 
 
@@ -14,7 +12,7 @@ public class S3Polling extends AbstractPolling<S3Client> {
     public S3Polling(Region region, Predicate<S3Client> condition) {
         super(S3Client.builder()
                 .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create("extra-qa"))
+                .credentialsProvider(DefaultCredentialsProvider.builder().profileName("extra-qa").build())
                 .build(),
                 condition
         );
