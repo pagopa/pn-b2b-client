e7c03abb8c62ec727c72a4f206be8306fcb1ce0c
Tullio Ranieri <ranieritullio@gmail.com>
2026-04-28 13:00:46 +0200

    feature: (QA-13904) Presa in carico bonaria - Temporany changed base url

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPaB2bInternalInformalClientImpl.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPaB2bInternalInformalClientImpl.java
@@ -27,7 +27,7 @@ public class PnPaB2bInternalInformalClientImpl {
 
     public PnPaB2bInternalInformalClientImpl(
             RestTemplate restTemplate,
-            @Value("${pn.internal.delivery-base-url}") String deliveryBasePath,
+            @Value("${pn.internal.delivery-push-base-url}") String deliveryBasePath,
             @Value("${pn.internal.pa-id}") String paId) {
 
         this.paId = paId;
