060dd447252723b086c7f30c2974c651c27b9a53
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-04-30 11:56:45 +0200

    feat(tracing-refactoring)[QA-13610]: commented a solution only for local run

--- interop-qa-tests/src/main/java/it/pagopa/interop/tracing/client/polling/S3Polling.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/tracing/client/polling/S3Polling.java
@@ -12,7 +12,13 @@ public class S3Polling extends AbstractPolling<S3Client> {
     public S3Polling(Region region, Predicate<S3Client> condition) {
         super(S3Client.builder()
                 .region(region)
-                .credentialsProvider(DefaultCredentialsProvider.builder().profileName("extra-qa").build())
+                // Per lanciare i test Tracing in locale serve aggiungere i token di accesso per Extra QA
+                // nel file .aws/credentials sotto il profilo [extra-qa] e non sotto [default] che è dedicato a QA.
+                // Decommentare in locale il metodo per leggere il profilo extra-qa.
+                .credentialsProvider(DefaultCredentialsProvider
+                        .builder()
+                        //.profileName("extra-qa")
+                        .build())
                 .build(),
                 condition
         );
