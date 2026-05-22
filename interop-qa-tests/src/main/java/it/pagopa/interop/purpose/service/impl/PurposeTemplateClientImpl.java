679ab37fd767b94f01b2794d37e27a768a277cb8
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-05-22 20:03:18 +0200

    feat(link-purpose-template-agevolata): [QA-14284] add method getPurposeTemplateLinkableResources client API

--- interop-qa-tests/src/main/java/it/pagopa/interop/purpose/service/impl/PurposeTemplateClientImpl.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/purpose/service/impl/PurposeTemplateClientImpl.java
@@ -179,4 +179,9 @@ public class PurposeTemplateClientImpl extends AbstractClient implements IPurpos
     public PurposeTemplates getPurposeTemplates(Integer offset, Integer limit, String purposeTitle, List<UUID> creatorIds, List<UUID> eserviceIds, List<it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeTemplateState> states, it.pagopa.interop.generated.openapi.clients.m2mGateway.model.TargetTenantKind targetTenantKind, Boolean handlesPersonalData) {
         return m2mPurposeTemplatesApi.getPurposeTemplates(offset, limit, purposeTitle, creatorIds, eserviceIds, states, targetTenantKind, handlesPersonalData);
     }
+
+    @Override
+    public LinkableResources getPurposeTemplateLinkableResources(UUID purposeTemplateId, Integer offset, Integer limit, String q, List<UUID> publisherIds) {
+        return purposesTemplateApi.getPurposeTemplateLinkableResources(purposeTemplateId, offset, limit, q, publisherIds);
+    }
 }
