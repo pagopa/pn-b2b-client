17e71412411bf236d4f7d78c890dee3e438690d6
Vincenzo Massaro <vincenzo.massaro@alten.it>
2026-05-27 10:52:04 +0200

    chore(InteropMaintenanceServiceImpl): [QA-13808] impl cambio tenant

--- interop-qa-tests/src/main/java/it/pagopa/interop/maintenance/InteropMaintenanceServiceImpl.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/maintenance/InteropMaintenanceServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 @Component
 public class InteropMaintenanceServiceImpl implements InteropMaintenanceService {
 
-    // TODO appare lecita un'astrazione ad hoc per TenantApi, cioè un client che gestisca quando usare token
+    // TODO appare lecita un'astrazione ad hoc per TenantApi di process, cioè un client che gestisca quando usare token
     //  bff e quando usare token maintenance
     private final TenantApi processBffTenantApi;
     private final TenantApi processMaintTenantApi;
@@ -58,34 +58,40 @@ public class InteropMaintenanceServiceImpl implements InteropMaintenanceService 
 
         bffApiClient.setBearerToken(tokenBff);
         System.out.println("Base path usato per get tenant: " + bffApiClient.getBasePath());
-        ResponseEntity<Tenant> tenant = processBffTenantApi.getTenantWithHttpInfo(
+        ResponseEntity<Tenant> processTenant = processBffTenantApi.getTenantWithHttpInfo(
                 xCorrelationId,
                 organizationId);
+        System.out.println("Prima della modifica, il tenant kind risulta essere: " + processTenant.getBody().getKind());
 
-        List<String> metadataVersion = tenant.getHeaders().get("X-Metadata-Version");
+        List<String> metadataVersion = processTenant.getHeaders().get("X-Metadata-Version");
         System.out.println("Header metadata version: " + metadataVersion);
 
         String currentVersion = metadataVersion.get(0);
         System.out.println("Header index 0: " + currentVersion);
 
         tenantsBffClient.setBearerToken(tokenBff);
-        it.pagopa.interop.generated.openapi.clients.bff.model.Tenant tenant1 = tenantsBffClient.getTenant(organizationId);
-        String selfcareInstitutionType = tenant1.getSelfcareInstitutionType();
+        it.pagopa.interop.generated.openapi.clients.bff.model.Tenant tenantFromBff = tenantsBffClient.getTenant(organizationId);
+        String selfcareInstitutionType = tenantFromBff.getSelfcareInstitutionType();
 
-        MaintenanceTenantUpdatePayload mapped = this.mapper.mapWith(tenant.getBody(), Integer.parseInt(currentVersion), TenantKind.valueOf(tenantKind), selfcareInstitutionType);
+        MaintenanceTenantUpdatePayload mapped = this.mapper.mapWith(processTenant.getBody(), Integer.parseInt(currentVersion), TenantKind.valueOf(tenantKind), selfcareInstitutionType);
         System.out.println("Tenant finale, prima dell'applicazione del cambio kind: ");
         System.out.println(mapped);
 
-        // TODO al momento resta scoperto il campo selfcareInstitutionType, che dobbiamo capire da dove ottenerlo, oppure
-        //  se non specificarlo affatto. Rif https://pagopaspa.slack.com/archives/C0ADC8WCYCW/p1779797267239509
-        // TODO resta scoperto anche currentVersion, l'header che hanno indicato non lo sto ricevendo (ricontrollare)
-
-
-
-        // mapping verso dto di creazione, impostando il tenantKind corretto (da parametro, non da output del passo precedente)
         // POST modifica tenant con polling fino a successo
+        // TODO coprire con polling sia questa che le altre chiamate
+        processMaintTenantApi.maintenanceTenantUpdate(xCorrelationId, organizationId, mapped);
+
+        // FIXME utile solo a fini di debug, si verifica che il tenant appena modificato differisca dal
+        //  precedente solo per il tenant kind e per il campo "updatedAt"
+        ResponseEntity<Tenant> processTenantPostKindUpdate = processBffTenantApi.getTenantWithHttpInfo(
+                xCorrelationId,
+                organizationId);
+        System.out.println("Dopo la modifica, il tenant kind risulta ora essere: " +  processTenantPostKindUpdate.getBody().getKind());
+        processTenantPostKindUpdate.getBody().setUpdatedAt(null);
+        processTenantPostKindUpdate.getBody().setKind(null);
+        processTenant.getBody().setUpdatedAt(null);
+        processTenant.getBody().setKind(null);
+        System.out.println("A meno di tenantKind e updatedAt le due versioni del tenant risultano uguali -> " + processTenant.getBody().equals(processTenantPostKindUpdate.getBody()));
     }
-
-
 
 }
