8c91dd3ba9ddeb53708934f3f7595e301823acd8
99niccoloCacace <niccolo.cacace@alten.it>
2026-05-27 16:39:54 +0200

    feat(manual-archiving): [QA-14294] add archiving states to descriptor status mapping

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/shared/EServiceTemplateParameterTypes.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/shared/EServiceTemplateParameterTypes.java
@@ -24,7 +24,7 @@ public class EServiceTemplateParameterTypes {
         };
     }
 
-    @ParameterType("DRAFT|PUBLISHED|DEPRECATED|SUSPENDED|ARCHIVED|WAITING_FOR_APPROVAL")
+    @ParameterType("DRAFT|PUBLISHED|DEPRECATED|SUSPENDED|ARCHIVED|WAITING_FOR_APPROVAL|ARCHIVING|ARCHIVING_SUSPENDED")
     public EServiceDescriptorState eServiceDescriptorState(String state) {
         return switch (state) {
             case "DRAFT"                -> EServiceDescriptorState.DRAFT;
@@ -33,6 +33,8 @@ public class EServiceTemplateParameterTypes {
             case "SUSPENDED"            -> EServiceDescriptorState.SUSPENDED;
             case "ARCHIVED"             -> EServiceDescriptorState.ARCHIVED;
             case "WAITING_FOR_APPROVAL" -> EServiceDescriptorState.WAITING_FOR_APPROVAL;
+            case "ARCHIVING"            -> EServiceDescriptorState.ARCHIVING;
+            case "ARCHIVING_SUSPENDED"  -> EServiceDescriptorState.ARCHIVING_SUSPENDED;
             default                     -> throw new IllegalArgumentException("Unsupported %s value: %s".formatted(
                 EServiceDescriptorState.class.getSimpleName(),
                 state));
