2c8dbf726ba7c9fc87f7fd4b4c700877130ac7c8
99niccoloCacace <niccolo.cacace@alten.it>
2026-05-27 16:40:38 +0200

    feat(manual-archiving): [QA-14294] add step to archive the old e-service version

---
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/DescriptorArchivingSteps.java
@@ -0,0 +1,41 @@
+package it.pagopa.pn.interop.cucumber.steps.catalog;
+
+import io.cucumber.java.en.When;
+import it.pagopa.interop.common.IHttpExecutor;
+import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
+import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
+import it.pagopa.pn.interop.cucumber.steps.catalog.utils.CatalogResolver;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+public class DescriptorArchivingSteps {
+    private final ClientTokenConfigurator clientTokenConfigurator;
+    private final SharedStepsContext sharedStepsContext;
+    private final IHttpExecutor httpCallExecutor;
+    private final CatalogResolver catalogResolver;
+
+    public DescriptorArchivingSteps(
+            ClientTokenConfigurator clientTokenConfigurator,
+            SharedStepsContext sharedStepsContext
+    ) {
+        this.clientTokenConfigurator = clientTokenConfigurator;
+        this.sharedStepsContext = sharedStepsContext;
+        this.httpCallExecutor = sharedStepsContext.getHttpCallExecutor();
+        this.catalogResolver = new CatalogResolver(sharedStepsContext);
+    }
+
+    @When("l'utente archivia la vecchia versione con id {string} dell'e-service con id {string}")
+    public void archiveOldDescriptor(String descriptorId, String eServiceId) {
+        clientTokenConfigurator.setBearerToken(sharedStepsContext.getUserToken());
+
+        UUID resolvedDescriptorId = catalogResolver.resolveOldDescriptorId(descriptorId);
+        UUID resolvedEServiceId = catalogResolver.resolveEServiceId(eServiceId);
+
+        httpCallExecutor.performCall(
+                () -> clientTokenConfigurator.getEServiceClient()
+                        .scheduleArchiveDescriptor(resolvedEServiceId, resolvedDescriptorId),
+                ResponseEntity::getStatusCode
+        );
+    }
+}
