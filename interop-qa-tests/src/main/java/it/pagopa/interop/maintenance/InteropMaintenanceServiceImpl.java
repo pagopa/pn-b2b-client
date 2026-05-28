aac1a68a31093b10405fd815e7a0710c30ee6fc7
Vincenzo Massaro <vincenzo.massaro@alten.it>
2026-05-28 22:46:40 +0200

    refactor(adeg-analisi-rischio): [QA-13808] aggiunto ITenantsProcessApi
    Logiche di comunicazione che le APIs di tenant-process raccolte in classe client ad hoc

--- interop-qa-tests/src/main/java/it/pagopa/interop/maintenance/InteropMaintenanceServiceImpl.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/maintenance/InteropMaintenanceServiceImpl.java
@@ -1,54 +1,42 @@
 package it.pagopa.interop.maintenance;
 
 import it.pagopa.interop.authorization.service.identity.IdentityService;
-import it.pagopa.interop.generated.openapi.clients.tenant_process.ApiClient;
-import it.pagopa.interop.generated.openapi.clients.tenant_process.api.TenantApi;
 import it.pagopa.interop.generated.openapi.clients.tenant_process.model.MaintenanceTenantUpdatePayload;
 import it.pagopa.interop.generated.openapi.clients.tenant_process.model.Tenant;
 import it.pagopa.interop.generated.openapi.clients.tenant_process.model.TenantKind;
 import it.pagopa.interop.tenant.service.ITenantsApi;
+import it.pagopa.interop.tenant.service.ITenantsProcessApi;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.UUID;
 
 @Component
 public class InteropMaintenanceServiceImpl implements InteropMaintenanceService {
-
-    // TODO appare lecita un'astrazione ad hoc per TenantApi di process, cioè un client che gestisca quando usare token
-    //  bff e quando usare token maintenance
-    private final TenantApi processBffTenantApi;
-    private final TenantApi processMaintTenantApi;
     private final ITenantsApi tenantsBffClient;
-    private final IdentityService identityService;
-    private final ApiClient bffApiClient;
-    private final ApiClient maintenanceApiClient;
+    private final ITenantsProcessApi tenantsProcessClient;
     private final TenantMapper mapper;
+    private final IdentityService identityService;
 
     public InteropMaintenanceServiceImpl(
             @Value("${TENANT_PROCESS_HOST}") String basePath,
-            RestTemplate restTemplate,
             @Qualifier("interopIdentityService") IdentityService identityService,
             TenantMapper mapper,
-            ITenantsApi tenantsBffClient
+            ITenantsApi tenantsBffClient,
+            ITenantsProcessApi tenantsProcessClient
     ) {
         System.out.println("HOST preso da envar: " + basePath);
         System.out.println("Contenuto della envar TENANT_PROCESS_HOST: " + System.getenv("TENANT_PROCESS_HOST"));
         System.out.println("Contenuto della property TENANT_PROCESS_HOST: " + System.getProperty("TENANT_PROCESS_HOST"));
         System.out.println("Contenuto della envar aCaso: " + System.getenv("aCaso"));
         System.out.println("Contenuto della envar GITHUB_ENV: " + System.getenv("GITHUB_ENV"));
-        bffApiClient = new ApiClient(restTemplate).setBasePath(basePath);
-        this.processBffTenantApi = new TenantApi(bffApiClient);
-        this.identityService = identityService;
-        maintenanceApiClient = new ApiClient(restTemplate).setBasePath(basePath);
-        maintenanceApiClient.setBearerToken(this.identityService::getMaintenanceToken);
-        this.processMaintTenantApi = new TenantApi(maintenanceApiClient);
         this.mapper = mapper;
+        this.identityService = identityService;
         this.tenantsBffClient = tenantsBffClient;
+        this.tenantsProcessClient = tenantsProcessClient;
     }
 
     @Override
@@ -57,9 +45,9 @@ public class InteropMaintenanceServiceImpl implements InteropMaintenanceService 
         String tokenBff = identityService.getToken(tenantAlias, "admin");
         UUID organizationId = identityService.getOrganizationId(tenantAlias);
 
-        bffApiClient.setBearerToken(tokenBff);
-        System.out.println("Base path usato per get tenant: " + bffApiClient.getBasePath());
-        ResponseEntity<Tenant> processTenant = processBffTenantApi.getTenantWithHttpInfo(
+        tenantsBffClient.setBearerToken(tokenBff);
+        tenantsProcessClient.setBearerToken(tokenBff);
+        ResponseEntity<Tenant> processTenant = tenantsProcessClient.getTenant(
                 xCorrelationId,
                 organizationId);
         TenantKind kindIniziale = processTenant.getBody().getKind();
@@ -81,12 +69,12 @@ public class InteropMaintenanceServiceImpl implements InteropMaintenanceService 
 
         // POST modifica tenant con polling fino a successo
         // TODO coprire con polling sia questa che le altre chiamate
-        processMaintTenantApi.maintenanceTenantUpdate(xCorrelationId, organizationId, mapped);
+        tenantsProcessClient.maintenanceTenantUpdate(xCorrelationId, organizationId, mapped);
 
         // FIXME utile solo a fini di debug, si verifica che il tenant appena modificato differisca dal
         //  precedente solo per il tenant kind e per il campo "updatedAt"
         sleep();
-        ResponseEntity<Tenant> processTenantPostKindUpdate = processBffTenantApi.getTenantWithHttpInfo(
+        ResponseEntity<Tenant> processTenantPostKindUpdate = tenantsProcessClient.getTenant(
                 xCorrelationId,
                 organizationId);
         System.out.println("Dopo la modifica, il tenant kind risulta ora essere: " +  processTenantPostKindUpdate.getBody().getKind());
