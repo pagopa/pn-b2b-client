c8549c325590ccacda520367b1206fc1206d6cab
99niccoloCacace <niccolo.cacace@alten.it>
2026-05-28 17:01:59 +0200

    feat(manual-archiving): [QA-14294] add step for full e-service archiving

---
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/EServiceArchivingSteps.java
@@ -0,0 +1,50 @@
+package it.pagopa.pn.interop.cucumber.steps.catalog;
+
+import io.cucumber.java.en.When;
+import it.pagopa.interop.common.IHttpExecutor;
+import it.pagopa.interop.generated.openapi.clients.bff.model.EServiceArchivingReasonSeed;
+import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
+import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
+import it.pagopa.pn.interop.cucumber.steps.catalog.utils.CatalogResolver;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+public class EServiceArchivingSteps {
+
+    private final ClientTokenConfigurator clientTokenConfigurator;
+    private final SharedStepsContext sharedStepsContext;
+    private final IHttpExecutor httpCallExecutor;
+    private final CatalogResolver catalogResolver;
+
+    public EServiceArchivingSteps(
+            ClientTokenConfigurator clientTokenConfigurator,
+            SharedStepsContext sharedStepsContext
+    ) {
+        this.clientTokenConfigurator = clientTokenConfigurator;
+        this.sharedStepsContext = sharedStepsContext;
+        this.httpCallExecutor = sharedStepsContext.getHttpCallExecutor();
+        this.catalogResolver = new CatalogResolver(sharedStepsContext);
+    }
+
+    @When("l'utente avvia il processo di archiviazione dell'e-service con id {string} e specificando la motivazione {string}")
+    public void scheduleEServiceArchiving(String eServiceId, String archivingReason) {
+        clientTokenConfigurator.setBearerToken(sharedStepsContext.getUserToken());
+
+        UUID resolvedEServiceId = catalogResolver.resolveEServiceId(eServiceId);
+        String resolvedArchivingReason = catalogResolver.resolveArchivingReason(archivingReason);
+
+        scheduleArchiveEService(resolvedEServiceId, resolvedArchivingReason);
+    }
+
+    private void scheduleArchiveEService(UUID eServiceId, String archivingReason) {
+        httpCallExecutor.performCall(
+                () -> clientTokenConfigurator.getEServiceClient()
+                        .scheduleArchiveEService(
+                                eServiceId,
+                                new EServiceArchivingReasonSeed().archivingReason(archivingReason)
+                        ),
+                ResponseEntity::getStatusCode
+        );
+    }
+}
