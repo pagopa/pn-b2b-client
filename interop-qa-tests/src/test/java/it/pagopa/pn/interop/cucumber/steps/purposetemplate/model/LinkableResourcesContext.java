2317c8d67426a5f92b6f8107565e830c2f540d65
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-06-08 20:49:24 +0200

    feat(link-purpose-template-agevolata): [QA-14284] improved shared context and attributes for M2M

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/purposetemplate/model/LinkableResourcesContext.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/purposetemplate/model/LinkableResourcesContext.java
@@ -2,10 +2,12 @@ package it.pagopa.pn.interop.cucumber.steps.purposetemplate.model;
 
 import it.pagopa.interop.generated.openapi.clients.bff.model.LinkableResources;
 import it.pagopa.interop.generated.openapi.clients.m2mGateway.model.PurposeTemplateState;
+import it.pagopa.interop.generated.openapi.clients.m2mGatewayV3.model.EServiceTemplates;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,14 +16,20 @@ import java.util.UUID;
 public class LinkableResourcesContext {
     private LinkableResources lastLinkableResources;
     private LinkableResources referenceLinkableResources;
-    private List<String> referenceEServiceTemplateNames;
-    private List<String> referenceEServiceNames;
-    private List<String> referenceResourceNames;
-    private List<UUID> referencePublisherIds;
-    private List<UUID> referenceEServiceTemplateIds;
-    private List<UUID> referenceEServiceIds;
+    private EServiceTemplates lastEServiceTemplates;
+    private EServiceTemplates referenceEServiceTemplates;
+    private List<String> referenceEServiceTemplateNames = new ArrayList<>();
+    private List<String> referenceEServiceNames = new ArrayList<>();
+    private List<String> referenceResourceNames = new ArrayList<>();
+    private List<UUID> referencePublisherIds = new ArrayList<>();
+    private List<UUID> referenceEServiceTemplateIds = new ArrayList<>();
+    private List<UUID> referenceEServiceIds = new ArrayList<>();
 
     public void saveLastLinkableResourcesAsAReference() {
         referenceLinkableResources = lastLinkableResources;
+    }
+
+    public void saveLastLinkableEServiceTemplatesAsAReference() {
+        referenceEServiceTemplates = lastEServiceTemplates;
     }
 }
