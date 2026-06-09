e1e41409a8727af3520aaf406201792882d26545
Emanuele Scozzafava <manu71@gmail.com>
2026-06-09 17:45:04 +0200

    feat(async-exchange): [QA-13754] add 'voucher_async' tag to AsyncExchangeTest class

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/AsyncExchangeTest.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/AsyncExchangeTest.java
@@ -12,6 +12,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," + "html:target/cucumber-report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.interop.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "same_thread")
-@IncludeTags({"e-service-template-async", "e-service-async"})
+@IncludeTags({"e-service-template-async", "e-service-async", "voucher_async"})
 public class AsyncExchangeTest extends AbstractEServiceTemplateTest {
 }
