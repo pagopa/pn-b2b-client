5b54adb2f07ae0668647822250bfe4010389ae01
Matteo Sperati <matteo.sperati@dgsspa.com>
2026-06-17 17:12:20 +0200

    feature(CorrezioneTimeline Fase3)[QA-14314] attempt to split scenarios for costs check of REWORK into multiple suites

--- src/test/java/it/pagopa/pn/cucumber/TimelineReworkF2CostiAsyncTest.java
+++ src/test/java/it/pagopa/pn/cucumber/TimelineReworkF2CostiAsyncTest.java
@@ -12,6 +12,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @ExcludeTags({"ignore"})
-@IncludeTags({"timelineReworkF2_costi"})
-public class TimelineReworkF2CostiTest {
+@IncludeTags({"timelineReworkF2_costi_async"})
+public class TimelineReworkF2CostiAsyncTest {
 }
