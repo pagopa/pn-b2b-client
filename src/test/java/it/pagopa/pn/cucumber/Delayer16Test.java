c6e1031b190382e02217488326d85d1252cebf74
mmancini95 <matteo.mancini@pagopa.it>
2026-06-19 16:58:50 +0200

    feat(Delayer16Test): [QA-14475] aggiornati i tag di inclusione rimuovendo i tag non necessari

--- src/test/java/it/pagopa/pn/cucumber/Delayer16Test.java
+++ src/test/java/it/pagopa/pn/cucumber/Delayer16Test.java
@@ -22,6 +22,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         value = "same_thread"
 )
 @ExcludeTags({"ignore"})
-@IncludeTags({"delayer16", "delayer4", "delayer3"})
+@IncludeTags({"delayer16"})
 public class Delayer16Test {
 }
