fb34fcb1c06ccc6fe886f0d288a139a18a8a85ed
Vincenzo Massaro <vincenzo.massaro@alten.it>
2026-06-24 16:35:33 +0200

    fix(adeg-analisi-rischio): spostata gestione concorrenza su singleton

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/maintenance/TenantSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/maintenance/TenantSteps.java
@@ -7,20 +7,16 @@ import it.pagopa.interop.maintenance.InteropMaintenanceService;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 @Slf4j
 public class TenantSteps {
     private final InteropMaintenanceService maintenanceService;
-
     private final SharedStepsContext sharedStepsContext;
+    private final TenantSetupState setupState;
 
-    private final ReentrantLock lock = new ReentrantLock();
-    private volatile boolean setupPerformed = false;
-
-    public TenantSteps(InteropMaintenanceService maintenanceService, SharedStepsContext sharedStepsContext) {
+    public TenantSteps(InteropMaintenanceService maintenanceService, SharedStepsContext sharedStepsContext, TenantSetupState setupState) {
         this.maintenanceService = maintenanceService;
         this.sharedStepsContext = sharedStepsContext;
+        this.setupState = setupState;
     }
 
     /* Resetta i tenant kind al loro valore "naturale", qualora in un'esecuzione precedente siano stati
@@ -37,21 +33,21 @@ public class TenantSteps {
         /* 29/05/2026 si effettua l'allineamento solo la prima volta, per correggere l'eventuale caso sfortuito
          * tale per cui un'esecuzione precedente della suite di "Adeguamento analisi del rischio" abbia
          * lasciato una condizione incoerente nei tenant kind. */
-        // Double-Checked Locking
-        if (!setupPerformed) {
-            lock.lock();
+        // Double-Checked Locking su stato condiviso tra tutte le istanze degli step
+        if (setupState.isSetupNotPerformed()) {
+            setupState.getLock().lock();
             try {
-                if (!setupPerformed) {
+                if (setupState.isSetupNotPerformed()) {
                     log.info("Aligning tenant kinds...");
                     maintenanceService.alignTenantKinds();
                     log.info("Tenant kinds aligned");
-                    setupPerformed = true;
+                    setupState.setSetupPerformed(true);
                 }
             } catch (Exception e) {
-                setupPerformed = false;
+                setupState.setSetupPerformed(false);
                 throw e;
             } finally {
-                lock.unlock();
+                setupState.getLock().unlock();
             }
         }
     }
@@ -80,3 +76,4 @@ public class TenantSteps {
         sharedStepsContext.setTenantType(kind);
     }
 }
+
