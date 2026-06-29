4ef20c93bb8e580ff18e5bc72a0680f6332aceef
Tullio Ranieri <ranieritullio@gmail.com>
2026-06-29 15:47:50 +0200

    feature:(hotfix-20479) added cost mode for vas.

--- src/test/java/it/pagopa/pn/cucumber/steps/pa/utilityVersions/B2bUtils.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/pa/utilityVersions/B2bUtils.java
@@ -393,7 +393,8 @@ public abstract class B2bUtils {
             responseOk = Integer.parseInt(parameters[2].split(":")[1]);
 
             costoBaseNotifica = Long.parseLong(getProperty(context, COSTO_BASE_NOTIFICA));
-            String propertyValue = getProperty(context, TECHNICAL_REFUSAL_COST_MODE);
+            //String propertyValue = getProperty(context, TECHNICAL_REFUSAL_COST_MODE);
+            String propertyValue = "RECIPIENT_BASED;100";
             String[] modeCost = (propertyValue != null) ? propertyValue.split(";") : null;
 
             if (modeCost == null || modeCost.length < 2 || responseKo == 0) {
