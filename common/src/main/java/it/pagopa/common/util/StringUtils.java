0a74fb1a3dff3cde7fa73569b390e5e422286721
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-06-30 18:14:56 +0200

    feat(common): method resolveDynamicValues to parse and solve $ functions in string

--- common/src/main/java/it/pagopa/common/util/StringUtils.java
+++ common/src/main/java/it/pagopa/common/util/StringUtils.java
@@ -1,8 +1,14 @@
 package it.pagopa.common.util;
 
+import it.pagopa.common.model.ISharedContext;
+
+import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,6 +40,11 @@ public class StringUtils {
     public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     /**
+     * Nome della funzione per recuperare valori dal contesto.
+     */
+    public static final String CONTEXT_FUNCTION_NAME = "CONTEXT";
+
+    /**
      * Pattern utilizzato per identificare i placeholder dinamici.
      * <p>
      * Esempi validi:
@@ -52,8 +63,7 @@ public class StringUtils {
     private static final Map<String, Supplier<String>> FUNCTIONS =
             Map.of(
                     "NULL", () -> null,
-                    "EMPTY", () -> "",
-                    "TODAY", () -> LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                    "EMPTY", () -> ""
             );
 
     /**
@@ -111,42 +121,63 @@ public class StringUtils {
         return value;
     }
 
-    public static String resolveContextualValue(String value, SharedStepsContext context) {
+    public static String resolveDynamicValues(String textTemplate, ISharedContext context) {
         StringBuilder text = new StringBuilder();
-        String functionName = "$DA_CONTESTO(";
+        String functionName, argument;
         int reachedIndex = 0;
-        int labelStartIndex = textTemplate.indexOf(functionName, reachedIndex);
-        int labelEndIndex;
-        while (labelStartIndex > -1) {
-            text.append(textTemplate.substring(reachedIndex, labelStartIndex));
-            labelStartIndex += functionName.length();
-            labelEndIndex = textTemplate.indexOf(')', labelStartIndex);
-            String label = textTemplate.substring(labelStartIndex, labelEndIndex);
+        int functionNameStart = textTemplate.indexOf("$", reachedIndex);
+        int functionNameEnd = textTemplate.indexOf("(", functionNameStart);
+        int argumentStart = 0, argumentEnd = 0;
+
+        while (functionNameStart > -1 && functionNameEnd > -1) {
             String value = "";
-            switch (label) {
-                case "agreementId": value = sharedStepsContext.getAgreementId().toString(); break;
-                case "eServiceName": value = sharedStepsContext.getEServicesCommonContext().getName(); break;
-                case "eServiceId": value = sharedStepsContext.getEServicesCommonContext().getEserviceId().toString(); break;
-                case "descriptorId": value = sharedStepsContext.getEServicesCommonContext().getDescriptorId().toString(); break;
-                case "oldDescriptorId": value = sharedStepsContext.getEServicesCommonContext().getOldDescriptorId().toString(); break;
-                case "producerName": value = sharedStepsContext.getEServicesCommonContext().getProducerName(); break;
-                case "consumerName": value = sharedStepsContext.getTenantCommonContext().getConsumerTenantName(); break;
-                // TODO Per ragioni di retrocompatibilità restano i valori TODAY e TODAY+GRACE_PERIOD
-                // ma non andrebbero risolti come valori contestuali, piuttosto risolti da DateUtils
-                case "TODAY": value = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")); break;
-                case "TODAY+GRACE_PERIOD":
-                    value = LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")); break;
-                default:
-                    throw new IllegalArgumentException("Not supported value '" + label + "' in function $" + functionName);
+
+            // Raccogli testo non coinvolto in una funzione
+            text.append(textTemplate.substring(reachedIndex, functionNameStart));
+
+            // Recupera il nome di una funzione
+            functionName = textTemplate.substring(functionNameStart + 1, functionNameEnd);
+
+            // Recupera l'argomento della funzione
+            argumentStart = functionNameEnd + 1;
+            argumentEnd = textTemplate.indexOf(')', functionNameEnd);
+            argument = textTemplate.substring(argumentStart, argumentEnd);
+
+            // Esegue il tipo di funzione riconosciuto:
+
+            // 1) Funzione di recupero di un valore dal contesto
+            if (functionName.equals(CONTEXT_FUNCTION_NAME)) {
+                String methodName = "get" + argument.substring(0, 1).toUpperCase() + argument.substring(1);
+                try {
+                    Method getterMethod = ISharedContext.class.getMethod(methodName);
+                    value = (String)getterMethod.invoke(context);
+
+                } catch (NoSuchMethodException e) {
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            // 2) Funzione su DateUtils
+            } else if (DateUtils.FUNCTIONS.containsKey(functionName)) {
+                value = DateUtils.FUNCTIONS.get(functionName).apply(argument);
+
+            // 3) Funzione su StringUtils
+            } else if (StringUtils.FUNCTIONS.containsKey(functionName)) {
+                value = StringUtils.FUNCTIONS.get(functionName).get();
+
+            // 4) Nessuna funzione riconosciuta: considera testo ordinario
+            } else {
+                text.append(textTemplate.substring(functionNameStart, argumentEnd + 1));
             }
             text.append(value);
-            // Controlla se c'è un prossimo placeholder
-            reachedIndex = labelEndIndex + 1;
-            labelStartIndex = textTemplate.indexOf(functionName, reachedIndex);
-            if (labelStartIndex == -1) {
-                text.append(textTemplate.substring(reachedIndex));
-            }
+
+            // Controlla se c'è una prossima funzione in stringa
+            reachedIndex = argumentEnd + 1;
+            functionNameStart = textTemplate.indexOf("$", reachedIndex);
+            functionNameEnd = textTemplate.indexOf("(", functionNameStart);
         }
+        text.append(textTemplate.substring(reachedIndex));
         if (text.isEmpty()) text.append(textTemplate);
         return text.toString();
     }
