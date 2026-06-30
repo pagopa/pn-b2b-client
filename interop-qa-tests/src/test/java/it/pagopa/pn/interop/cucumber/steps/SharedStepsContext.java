b91b09371c581f1b79442a5a3653b75eab0055fe
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-06-30 18:23:26 +0200

    feat(notifiche-debito-tecnico): [QA-14309] implemented ISharedContext for SharedStepsContext

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/SharedStepsContext.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/SharedStepsContext.java
@@ -12,6 +12,7 @@ import it.pagopa.pn.interop.cucumber.steps.common.*;
 import it.pagopa.pn.interop.cucumber.steps.e_service_template.shared.EServiceTemplateStepContext;
 import it.pagopa.pn.interop.cucumber.steps.notification.model.NotificationCommonContext;
 import it.pagopa.pn.interop.cucumber.utility.delay_service.DelayService;
+import it.pagopa.common.model.ISharedContext;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -28,7 +29,7 @@ import java.util.regex.Pattern;
 @Setter
 @Slf4j
 @ScenarioScope
-public class SharedStepsContext {
+public class SharedStepsContext implements ISharedContext {
     private final IHttpExecutor httpCallExecutor;
     private final IdentityService identityService;
     private final PollingService pollingService;
@@ -107,4 +108,48 @@ public class SharedStepsContext {
         return scenarioId;
     }
 
+    @Override
+    public String getAgreementId() {
+        return this.agreementCommonContext.getAgreementId().toString();
+    }
+
+    @Override
+    public String getEServiceName() {
+        return this.eServicesCommonContext.getName();
+    }
+
+    @Override
+    public String getEServiceId() {
+        return this.eServicesCommonContext.getEserviceId().toString();
+    }
+
+    @Override
+    public String getDescriptorId() {
+        return this.eServicesCommonContext.getDescriptorId().toString();
+    }
+
+    @Override
+    public String getOldDescriptorId() {
+        return this.eServicesCommonContext.getOldDescriptorId().toString();
+    }
+
+    @Override
+    public String getProducerName() {
+        return this.eServicesCommonContext.getProducerName();
+    }
+
+    @Override
+    public String getConsumerName() {
+        return this.tenantCommonContext.getConsumerTenantName();
+    }
+
+    @Override
+    public String getPurposeId() {
+        return this.purposeCommonContext.getNewPurposeId().toString();
+    }
+
+    @Override
+    public String getPurposeTitle() {
+        return this.purposeCommonContext.getCreatedPurposes().get(0).getTitle();
+    }
 }
