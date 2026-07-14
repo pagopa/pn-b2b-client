a4e13345e78fdd099afcf4e65479ec0203bf3f91
mmancini95 <matteo.mancini@pagopa.it>
2026-07-14 16:18:37 +0200

    feature:[QA-15580] Aggiunta la classe MandateContext e aggiornato SendSharedContext per gestire il contesto delle deleghe

--- src/test/java/it/pagopa/pn/cucumber/steps/SendSharedContext.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/SendSharedContext.java
@@ -3,6 +3,7 @@ package it.pagopa.pn.cucumber.steps;
 import io.cucumber.java.Before;
 import io.cucumber.spring.ScenarioScope;
 import it.pagopa.pn.cucumber.steps.common.InformalNotificationContext;
+import it.pagopa.pn.cucumber.steps.common.MandateContext;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 @ScenarioScope
 public class SendSharedContext {
     private InformalNotificationContext informalNotificationContext;
+    private MandateContext mandateContext;
 
     @Before
     public void resetSharedStepsContext() {
         informalNotificationContext = new InformalNotificationContext();
+        mandateContext = new MandateContext();
     }
 }
