050fe43f239c3ee9f63b943f2d7d075986f2135d
Emanuele Scozzafava <manu71@gmail.com>
2026-07-30 18:05:50 +0200

    feat(certified-discrete-attr): [QA-16072] enhance certified discrete attribute creation with polling and response validation

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/attribute/CertifiedAttributeCreationSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/attribute/CertifiedAttributeCreationSteps.java
@@ -11,6 +11,10 @@ import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import it.pagopa.pn.interop.cucumber.steps.datapreparationservice.BFFDataPreparationService;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 public class CertifiedAttributeCreationSteps {
     private final SharedStepsContext sharedStepsContext;
@@ -18,6 +22,7 @@ public class CertifiedAttributeCreationSteps {
     private final IHttpExecutor httpCallExecutor;
     private final BFFDataPreparationService dataPreparationService;
     private String lastAttributeName;
+    private UUID lastAttributeId;
 
     public CertifiedAttributeCreationSteps(
         ClientTokenConfigurator clientTokenConfigurator,
@@ -33,11 +38,13 @@ public class CertifiedAttributeCreationSteps {
     @When("l'utente crea un attributo certificato")
     public void createCertifiedAttribute() {
         clientTokenConfigurator.setBearerToken(sharedStepsContext.getUserToken());
-        this.lastAttributeName = "new certified attribute %d".formatted(RandomUtils.insecure().randomInt());
         httpCallExecutor.performCall(() -> clientTokenConfigurator.getAttributeApiClient().createCertifiedAttributeRE(
             new CertifiedAttributeSeed()
                 .name("new certified attribute %d".formatted(RandomUtils.insecure().randomInt()))
                 .description("description test")));
+        if (httpCallExecutor.getResponseStatus().is2xxSuccessful()) {
+            getLastAttributeCreated(((ResponseEntity<Attribute>) httpCallExecutor.getResponse()).getBody().getId());
+        }
     }
 
     @When("l'utente crea {int} attributi certificati con successo")
@@ -60,15 +67,26 @@ public class CertifiedAttributeCreationSteps {
         );
 
         if (httpCallExecutor.getResponseStatus().is2xxSuccessful()) {
-            Attribute createdAttribute = (Attribute) httpCallExecutor.getResponse();
-            Assertions.assertNotNull(createdAttribute);
-            Assertions.assertNotNull(createdAttribute.getId());
-            Assertions.assertTrue(createdAttribute.getCode() != null && !createdAttribute.getCode().isEmpty());
-            Assertions.assertEquals(AttributeKind.CERTIFIED_DISCRETE, createdAttribute.getKind());
-            Assertions.assertEquals(seed.getName(), createdAttribute.getName());
-            Assertions.assertEquals(seed.getDescription(), createdAttribute.getDescription());
-            Assertions.assertNotNull(createdAttribute.getOrigin());
-            Assertions.assertNotNull(createdAttribute.getCreationTime());
+
+            UUID attributeId = ((Attribute) httpCallExecutor.getResponse()).getId();
+            sharedStepsContext.getPollingService().makePolling(
+                    () -> httpCallExecutor.performCall(
+                            () -> clientTokenConfigurator.getAttributeApiClient().getAttributeById(attributeId)),
+                    HttpStatus::is2xxSuccessful,
+                    "L'attributo non è stato creato correttamente!");
+
+            Attribute attribute = ((Attribute) httpCallExecutor.getResponse());
+            Assertions.assertNotNull(attribute);
+            Assertions.assertNotNull(attribute.getId());
+            Assertions.assertTrue(attribute.getCode() != null && !attribute.getCode().isEmpty());
+            Assertions.assertEquals(AttributeKind.CERTIFIED_DISCRETE, attribute.getKind());
+            Assertions.assertEquals(seed.getName(), attribute.getName());
+            Assertions.assertEquals(seed.getDescription(), attribute.getDescription());
+            Assertions.assertNotNull(attribute.getOrigin());
+            Assertions.assertNotNull(attribute.getCreationTime());
+
+            this.lastAttributeName = attribute.getName();
+            this.lastAttributeId = attribute.getId();
         }
     }
 
@@ -83,6 +101,8 @@ public class CertifiedAttributeCreationSteps {
             () -> clientTokenConfigurator.getAttributeApiClient().createCertifiedDiscreteAttribute(seed)
         );
         Assertions.assertTrue(httpCallExecutor.getResponseStatus().is2xxSuccessful());
+        Attribute attribute = ((Attribute) httpCallExecutor.getResponse());
+        getLastAttributeCreated(attribute.getId());
         httpCallExecutor.performCall(
             () -> clientTokenConfigurator.getAttributeApiClient().createCertifiedDiscreteAttribute(seed)
         );
@@ -92,10 +112,21 @@ public class CertifiedAttributeCreationSteps {
     public void attemptToCreateCertifiedDiscreteAttributeWithSameNameAsLastCreated() {
         clientTokenConfigurator.setBearerToken(sharedStepsContext.getUserToken());
         AttributeSeed seed = new AttributeSeed()
-            .name(lastAttributeName)
+            .name(this.lastAttributeName)
             .description("description test");
         httpCallExecutor.performCall(
             () -> clientTokenConfigurator.getAttributeApiClient().createCertifiedDiscreteAttribute(seed)
         );
+    }
+
+    private void getLastAttributeCreated(UUID attributeId) {
+        sharedStepsContext.getPollingService().makePolling(
+                () -> httpCallExecutor.performCall(
+                        () -> clientTokenConfigurator.getAttributeApiClient().getAttributeById(attributeId)),
+                res -> res != HttpStatus.INTERNAL_SERVER_ERROR,
+                "L'attributo non è stato creato correttamente!");
+        Attribute attribute = (Attribute) httpCallExecutor.getResponse();
+        this.lastAttributeName = attribute.getName();
+        this.lastAttributeId = attribute.getId();
     }
 }
