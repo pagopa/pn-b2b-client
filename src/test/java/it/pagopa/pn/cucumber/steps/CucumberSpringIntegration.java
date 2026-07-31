80233d08f73636158932af22a677804de7ec9b8a
Massimo G. D'Avanzo <massimo@tech-talents.ai>
2026-07-31 14:51:48 +0200

    test avanzamento notifiche async pnPolling implementato fix

--- src/test/java/it/pagopa/pn/cucumber/steps/CucumberSpringIntegration.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/CucumberSpringIntegration.java
@@ -17,6 +17,7 @@ import it.pagopa.pn.client.b2b.pa.parsing.config.PnLegalFactTokens;
 import it.pagopa.pn.client.b2b.pa.parsing.parser.impl.PnParser;
 import it.pagopa.pn.client.b2b.pa.parsing.service.impl.PnParserService;
 import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingFactory;
+import it.pagopa.pn.client.b2b.pa.polling.impl.PnPollingServicePaymentInfo;
 import it.pagopa.pn.client.b2b.pa.polling.impl.v1.PnPollingServiceStatusRapidV1;
 import it.pagopa.pn.client.b2b.pa.polling.impl.v1.PnPollingServiceStatusSlowV1;
 import it.pagopa.pn.client.b2b.pa.polling.impl.v1.PnPollingServiceTimelineRapidV1;
@@ -225,6 +226,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         PnB2bClientTimingConfigs.class,
         PnIOConnectorClientImpl.class,
         PnPollingFactory.class,
+        PnPollingServicePaymentInfo.class,
         CieGeneratorTool.class,
         TaxIdConfiguration.class,
 
