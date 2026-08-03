fc1f2915b2b9858ec57a1fd807a21ed1695c4b1e
mmancini95 <matteo.mancini@pagopa.it>
2026-08-03 12:58:23 +0200

    feat(paper-tracker): [QA-16452] aggiunti tag di esclusione per i test OCR

--- src/test/java/it/pagopa/pn/cucumber/PaperTrackerTest.java
+++ src/test/java/it/pagopa/pn/cucumber/PaperTrackerTest.java
@@ -13,6 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
 @IncludeTags({"paperTrackerRunMode890"})
+@ExcludeTags({"ocrRun", "ocrEnabled"})
 public class PaperTrackerTest {
 
 }
