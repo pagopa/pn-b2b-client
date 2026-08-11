5759966dcea08d0a0085d5aa0a170d0302d20dde
Vincenzo-Massaro <vincenzo.massaro@alten.it>
2026-08-11 11:42:58 +0200

    feature(cdt): [QA-16648] runners

---
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/HotfixTest_2_22.java
@@ -0,0 +1,10 @@
+package it.pagopa.pn.interop.cucumber;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({HotfixTest_2_22v2.class, HotfixTest_2_22v3.class})
+public class HotfixTest_2_22 {
+}
+
