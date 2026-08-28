3dec2fc6946a7add0073d5c1895d3ef15956d18d
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-08-28 17:50:51 +0200

    feat(info-template-get-e-service): [QA-16638] prepared check of templateRef but requires tuning

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/DescriptorReadConsumerSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/DescriptorReadConsumerSteps.java
@@ -10,6 +10,7 @@ import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 @Slf4j
@@ -56,9 +57,56 @@ public class DescriptorReadConsumerSteps {
         Assertions.assertNotNull(obj, "Response of e-service descriptor from catalog is null");
         boolean foundTemplateRef = false;
         try {
-            obj.getClass().getMethod("getTemplateRef");
+            Object templateRefObj = null;
+            Method method = obj.getClass().getMethod("getTemplateRef");
+            templateRefObj = method.invoke(obj);
+
+            method = templateRefObj.getClass().getMethod("getTemplateId");
+            String actualTemplateId = (String)method.invoke(templateRefObj);
+            Assertions.assertEquals(
+                    actualTemplateId,
+                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getId().toString()
+            );
+
+            method = templateRefObj.getClass().getMethod("getTemplateVersionId");
+            String actualTemplateVersionId = (String)method.invoke(templateRefObj);
+            Assertions.assertEquals(
+                    actualTemplateVersionId,
+                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getLastVersionId().toString()
+            );
+
+            method = templateRefObj.getClass().getMethod("getTemplateName");
+            String actualTemplateName = (String)method.invoke(templateRefObj);
+            Assertions.assertEquals(
+                    actualTemplateName,
+                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getName()
+            );
+
+            method = templateRefObj.getClass().getMethod("getTemplateInterface");
+            Object actualTemplateInterface = method.invoke(templateRefObj);
+            Assertions.assertNotNull(actualTemplateInterface);
+
+            method = templateRefObj.getClass().getMethod("getInterfaceMetadata");
+            Object actualInterfaceMetadata = method.invoke(templateRefObj);
+            Assertions.assertNotNull(actualInterfaceMetadata);
+
+            method = templateRefObj.getClass().getMethod("isNewTemplateVersionAvailable");
+            Object isNewTemplateVersionAvailable = method.invoke(templateRefObj);
+            Assertions.assertNotNull(isNewTemplateVersionAvailable);
+
+            method = templateRefObj.getClass().getMethod("getTemplateDailyCallsPerConsumer");
+            String templateDailyCallsPerConsumer = (String)method.invoke(templateRefObj);
+            Assertions.assertNotNull(templateDailyCallsPerConsumer);
+
+            method = templateRefObj.getClass().getMethod("getTemplateDailyCallsTotal");
+            String templateDailyCallsTotal = (String)method.invoke(templateRefObj);
+            Assertions.assertNotNull(templateDailyCallsTotal);
+
             foundTemplateRef = true;
-        } catch (NoSuchMethodException e) {}
+
+        } catch (NoSuchMethodException e) {
+        } catch (ReflectiveOperationException e) {
+        }
 
         if (templateRefNot.isEmpty()) {
             if (foundTemplateRef) {
