5e2003ced7819d0864e60a739691730aee8909a5
Emanuele Scozzafava <manu71@gmail.com>
2026-09-03 09:21:26 +0200

    fix(certified-discrete-attrs): [QA-16615] remove unused RestClientException import

--- interop-qa-tests/src/main/java/it/pagopa/interop/agreement/service/impl/M2MV3TenantClientImpl.java
+++ interop-qa-tests/src/main/java/it/pagopa/interop/agreement/service/impl/M2MV3TenantClientImpl.java
@@ -23,7 +23,6 @@ import lombok.ToString;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 
 @ToString
 @EqualsAndHashCode
