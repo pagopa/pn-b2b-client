f89a3e86d487c5c70150e2442919424dd9148875
stevesielinou <steve.sielinou@alten.it>
2026-09-04 09:10:28 +0200

    test(post-payment-workflow): [PN-21168] add post-payment workflow scenarios

---
+++ src/test/java/it/pagopa/pn/cucumber/PostPaymentWorkflowTest.java
@@ -0,0 +1,27 @@
+package it.pagopa.pn.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("it/pagopa/pn/cucumber/workflowNotifica/e2e/NotificationPaid.feature")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "json:target/cucumber-report.json,html:target/cucumber-report.html"
+)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
+@ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent")
+@IncludeTags("postPaymentWorkflow")
+@ExcludeTags("ignore")
+public class PostPaymentWorkflowTest {
+}
