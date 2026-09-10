3243e7baa8ad2771259c9c8f49831e47513c8ec2
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-09-10 11:11:33 +0200

    fix(info-template-get-e-service): [QA-16638] in DEV env description was required for e-service template

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
@@ -284,7 +284,8 @@ public class EServiceTemplateCreateSteps {
     private EServiceTemplateSeed getEServiceTemplateSeed(EServiceMode eServiceMode, Boolean flagPersonalData) {
         String templateName = testAssistant.buildEServiceTemplateName();
         VersionSeedForEServiceTemplateCreation version = new VersionSeedForEServiceTemplateCreation()
-                .voucherLifespan(86400);
+                .voucherLifespan(86400)
+                .description("Nuova versione");
         return new EServiceTemplateSeed()
                 .intendedTarget("Audience description per il template " + templateName)
                 .name(templateName)
