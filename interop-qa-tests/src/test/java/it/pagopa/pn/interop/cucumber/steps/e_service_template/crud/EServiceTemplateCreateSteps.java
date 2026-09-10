fe3d67446a49cdedbead90c2cd209d300e4d4a64
Emanuele Scozzafava <manu71@gmail.com>
2026-09-10 16:57:50 +0200

    feat(EServiceTemplateCreateSteps): [PIN-9782] [QA-17156] add version description for service template

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
@@ -283,6 +283,7 @@ public class EServiceTemplateCreateSteps {
     private EServiceTemplateSeed getEServiceTemplateSeed(EServiceMode eServiceMode, Boolean flagPersonalData) {
         String templateName = testAssistant.buildEServiceTemplateName();
         VersionSeedForEServiceTemplateCreation version = new VersionSeedForEServiceTemplateCreation()
+                .description("Descrizione della versione del servizio associato al template " + templateName)
                 .voucherLifespan(86400);
         return new EServiceTemplateSeed()
                 .intendedTarget("Audience description per il template " + templateName)
