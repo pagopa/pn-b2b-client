d4b49d410d008c0a39a087c3a1e027fd153ac5ac
Silvano Gresta Zucchi <silvano.gresta.ext@alten.it>
2026-09-10 11:03:28 +0200

    fix(info-template-get-e-service): [QA-16638] revert of detailed check of templateRef fixed actual and expected value positions added argument prevTemplateVersion through the gherkin expression

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/DescriptorReadConsumerSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/catalog/DescriptorReadConsumerSteps.java
@@ -5,14 +5,12 @@ import static org.assertj.core.api.Assertions.fail;
 import io.cucumber.java.en.When;
 import it.pagopa.interop.common.IHttpExecutor;
 import it.pagopa.interop.generated.openapi.clients.bff.model.CatalogEServiceDescriptor;
-import it.pagopa.interop.generated.openapi.clients.bff.model.EServiceDoc;
 import it.pagopa.pn.interop.cucumber.steps.ClientTokenConfigurator;
 import it.pagopa.pn.interop.cucumber.steps.SharedStepsContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -48,24 +46,22 @@ public class DescriptorReadConsumerSteps {
         );
     }
 
-    @When("^l'utente legge da catalogo i?l'? ?(ultimo|vecchio) descrittore e-service (senza|con) riferimenti al template$")
-    public void readEServiceDescriptorFromCatalogueAndCheckTemplateInfo(String descriptorQualifier, String templateRefWith) {
-        readEServiceDescriptorFromCatalogueAndCheckTemplateInfo(descriptorQualifier, templateRefWith, Map.of());
-    }
-
-    @When("^l'utente legge da catalogo i?l'? ?(ultimo|vecchio) descrittore e-service (senza|con) riferimenti al template e dati:$")
-    public void readEServiceDescriptorFromCatalogueAndCheckTemplateInfo(String descriptorQualifier, String templateRefWith, Map<String, Boolean> expectedData) {
+    @When("^l'utente legge da catalogo i?l'? ?(ultimo|vecchio) descrittore e-service (senza|con) riferimenti al(|la precedente versione del) template$")
+    public void readEServiceDescriptorFromCatalogueAndCheckTemplateInfo(String descriptorQualifier, String templateRefWith, String prevTemplateVersion) {
+        UUID expectedTemplateVersionId = sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getLastVersionId();
         if (descriptorQualifier.equals("vecchio")) {
             requireOldDescriptorRead();
+            expectedTemplateVersionId = sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getOldVersionId();
         } else {
             requireLastDescriptorRead();
+            if (!prevTemplateVersion.isEmpty()) {
+                expectedTemplateVersionId = sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getOldVersionId();
+            }
         }
         CatalogEServiceDescriptor obj = ((CatalogEServiceDescriptor)httpCallExecutor.getResponse());
         Assertions.assertNotNull(obj, "Response of e-service descriptor from catalog is null");
 
         boolean foundTemplateRef = false;
-        boolean expectedValueOfNewTemplateVersionAvailable =
-                expectedData.getOrDefault("isNewTemplateVersionAvailable", false);
 
         try {
             Object templateRefObj;
@@ -78,82 +74,23 @@ public class DescriptorReadConsumerSteps {
             method = templateRefObj.getClass().getMethod("getTemplateId");
             String actualTemplateId = method.invoke(templateRefObj).toString();
             Assertions.assertEquals(
+                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getId().toString(),
                     actualTemplateId,
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getId().toString()
+                    "templateId"
             );
 
             method = templateRefObj.getClass().getMethod("getTemplateVersionId");
-            String actualTemplateVersionId = method.invoke(templateRefObj).toString();
-            Assertions.assertEquals(
-                    actualTemplateVersionId,
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getLastVersionId().toString()
-            );
+            UUID actualTemplateVersionId = (UUID)method.invoke(templateRefObj);
+
+            Assertions.assertEquals(expectedTemplateVersionId, actualTemplateVersionId, "templateVersionId");
 
             method = templateRefObj.getClass().getMethod("getTemplateName");
             String actualTemplateName = (String)method.invoke(templateRefObj);
             Assertions.assertEquals(
+                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getName(),
                     actualTemplateName,
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getName()
+                    "templateName"
             );
-
-            method = templateRefObj.getClass().getMethod("getTemplateInterface");
-            Object actualTemplateInterface = method.invoke(templateRefObj);
-            Assertions.assertNotNull(actualTemplateInterface, "templateInterface is null");
-
-            EServiceDoc expectedInterface =
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getTemplateInterface();
-
-            method = actualTemplateInterface.getClass().getMethod("getPrettyName");
-            String prettyName = (String)method.invoke(actualTemplateInterface);
-            Assertions.assertEquals(expectedInterface.getPrettyName(), prettyName);
-
-            method = actualTemplateInterface.getClass().getMethod("getChecksum");
-            String checksum = (String)method.invoke(actualTemplateInterface);
-            Assertions.assertEquals(expectedInterface.getChecksum(), checksum);
-
-            method = templateRefObj.getClass().getMethod("getInterfaceMetadata");
-            Object actualInterfaceMetadata = method.invoke(templateRefObj);
-            Assertions.assertNotNull(actualInterfaceMetadata, "interfaceMetadata is null");
-
-            method = actualInterfaceMetadata.getClass().getMethod("getContactName");
-            String contactName = (String)method.invoke(actualInterfaceMetadata);
-            Assertions.assertEquals(
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getContactName(),
-                    contactName
-            );
-            method = actualInterfaceMetadata.getClass().getMethod("getContactEmail");
-            String contactEmail = (String)method.invoke(actualInterfaceMetadata);
-            Assertions.assertEquals(
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateManaged().getContactEmail(),
-                    contactEmail
-            );
-
-            method = templateRefObj.getClass().getMethod("isNewTemplateVersionAvailable");
-            Object isNewTemplateVersionAvailable = method.invoke(templateRefObj);
-            Assertions.assertEquals(
-                    expectedValueOfNewTemplateVersionAvailable,
-                    isNewTemplateVersionAvailable
-            );
-
-            Integer expectedDailyCallsPerConsumer =
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateVersionUpdateSeed()
-                            .getDailyCallsPerConsumer();
-
-            if (expectedDailyCallsPerConsumer != null) {
-                method = templateRefObj.getClass().getMethod("getTemplateDailyCallsPerConsumer");
-                Integer templateDailyCallsPerConsumer = (Integer)method.invoke(templateRefObj);
-                Assertions.assertEquals(expectedDailyCallsPerConsumer, templateDailyCallsPerConsumer);
-            }
-
-            Integer expectedDailyCallsTotal =
-                    sharedStepsContext.getEServiceTemplateStepContext().getLastTemplateVersionUpdateSeed()
-                            .getDailyCallsTotal();
-
-            if (expectedDailyCallsTotal != null) {
-                method = templateRefObj.getClass().getMethod("getTemplateDailyCallsTotal");
-                Integer templateDailyCallsTotal = (Integer)method.invoke(templateRefObj);
-                Assertions.assertEquals(expectedDailyCallsTotal, templateDailyCallsTotal);
-            }
 
             foundTemplateRef = true;
 
