6e0ebd4ed16deb06b055740e3775e128993181c0
Emanuele Scozzafava <manu71@gmail.com>
2026-09-10 18:29:29 +0200

    feat(EServiceTemplateCreateSteps): [PIN-9782] [QA-17156] add step to create e-service template with version description and corresponding feature scenario with BFF API

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/e_service_template/crud/EServiceTemplateCreateSteps.java
@@ -94,6 +94,19 @@ public class EServiceTemplateCreateSteps {
         testAssistant.mutateLastVersionState(desiredState);
     }
 
+    @When("l'utente effettua la creazione di un e-service template con la descrizione della versione impostata a {string}")
+    public void createEServiceTemplateWithDescription(String versionDescriptionContent) {
+        EServiceTemplateSeed templateSeed = this.getEServiceTemplateSeed(EServiceMode.DELIVER);
+        String versionDescription = switch (versionDescriptionContent) {
+            case "%null%" -> null;
+            case "%empty%" -> "";
+            default -> versionDescriptionContent;
+        };
+        assert templateSeed.getVersion() != null;
+        templateSeed.getVersion().setDescription(versionDescription);
+        this.createEServiceTemplate(templateSeed);
+    }
+
     @When("l'utente effettua la creazione di un e-service template {isAsynchronous} in modalità {eServiceMode} con tecnologia {string} in stato di {eServiceTemplateVersionState}")
     public void createEServiceTemplate(Boolean isAsync, EServiceMode eServiceMode, String technology, EServiceTemplateVersionState desiredState) {
         EServiceTechnology technology1 = EServiceTechnology.fromValue(technology);
