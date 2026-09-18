01c1685f181fb3285ca01d67e78712e497b945ca
Emanuele Scozzafava <manu71@gmail.com>
2026-09-18 17:51:47 +0200

    feat(TenantAssignCertifiedAttributeSteps): [PIN-10735] [QA-17106] update tenant certified attribute assignment to use organization ID

--- interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/tenant/TenantAssignCertifiedAttributeSteps.java
+++ interop-qa-tests/src/test/java/it/pagopa/pn/interop/cucumber/steps/tenant/TenantAssignCertifiedAttributeSteps.java
@@ -212,7 +212,7 @@ public class TenantAssignCertifiedAttributeSteps {
 
     @When("l'utente tenta di modificare l'attributo certificato discreto di {string} utilizzando l'ID dell'attributo certificato creato")
     public void updateTenantCertifiedAttributeWithInvaildAttributeKind(String tenantType) {
-        UUID tenantId = UUID.randomUUID();
+        UUID tenantId = identityService.getOrganizationId(tenantType);
         var certifiedAttribute = sharedStepsContext.getAttributeCommonContext().getCreatedAttributes().get(
                 sharedStepsContext.getAttributeCommonContext().getCreatedAttributes().size() - 1
         );
